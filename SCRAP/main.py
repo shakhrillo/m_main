168f8eeb55610e4e8b25cb98659fa703aab7b177
shakhrillo <mr.jhon1327@gmail.com>
2024-07-09 12:57:29 +0400

    Updated changes

--- SCRAP/main.py
+++ SCRAP/main.py
@@ -13,42 +13,33 @@ conn = mysql.connector.connect(
 cursor = conn.cursor()
 
 google_reviews = google_map_reviews(
-    url='https://www.google.com/maps/place/Frontera+Mexico%2FGuatemala/@14.9663853,-92.1471757,16.76z/data=!4m17!1m8!3m7!1s0x858e0b33fe4be03d:0xcdceec5f02680797!2s30875+Talism%C3%A1n,+Chis.,+Mexico!3b1!8m2!3d14.9633299!4d-92.14722!16s%2Fg%2F11c5m4dmbn!3m7!1s0x858e0bd22ad2597d:0x196210b7563412c4!8m2!3d14.9653625!4d-92.1466094!9m1!1b1!16s%2Fg%2F11kqr2vbys?entry=ttu'
+    url='https://www.google.com/maps/place/Control+Needle+North+Customs/@11.213892,-85.6137899,17.85z/data=!4m17!1m8!3m7!1s0x858e0b33fe4be03d:0xcdceec5f02680797!2s30875+Talism%C3%A1n,+Chis.,+Mexico!3b1!8m2!3d14.9633299!4d-92.14722!16s%2Fg%2F11c5m4dmbn!3m7!1s0x8f75a9f850f54cfb:0x43f908dacf7bb17c!8m2!3d11.215153!4d-85.6122499!9m1!1b1!16s%2Fg%2F11fq2tx58v?entry=ttu'
 )
 
-response_format = """
-    id SERIAL PRIMARY KEY,
-    border_name VARCHAR(255),
-    border_location VARCHAR(255),
-    border_country VARCHAR(255),
-    wait_time VARCHAR(255),
-    required_documents VARCHAR(255),
-    roads_conditions VARCHAR(255),
-    restaurants VARCHAR(255),
-    restrooms VARCHAR(255),
-    children_playground VARCHAR(255),
-    money_exchange VARCHAR(255),
-    atm VARCHAR(255),
-    nearest_hotel VARCHAR(255),
-    attentions VARCHAR(255),
-    bribes VARCHAR(255),
-    security VARCHAR(255)
-"""
+reviews = []
+for review in google_reviews:
+    openairesponse = send_ai_message(
+        model='gpt-3.5-turbo',
+        response_format={ "type": "json_object" },
+        sys_message='Give short sentiment review',
+        user_messages=[
+            " ".join(review),
+            "Give as json format"
+            # "Give a short review if it is an actual review of experience. Otherwise skip it. The result should be as json format. {'location', 'issues', 'suggestions', 'observations', 'recommendations', 'concerns', 'feedback', 'rating'}"
+        ]
+    )
 
-openairesponse = send_ai_message(
-    model='gpt-3.5-turbo',
-    response_format={ "type": "json_object" },
-    sys_message='Extract the sentiment from the review:',
-    user_messages=[
-        " ".join(google_reviews),
-        "Give the result as json format and include these values" + response_format + " in order to store the response in the database."
-    ]
-)
+    reviews.append(openairesponse)
+    send_telegram_message(
+        user_name='shakhrillo',
+        message=openairesponse
+    )
+
 
 # Add the response to the database
 # cursor.execute(f"INSERT INTO borders (border_name, border_location, border_country, wait_time, required_documents, roads_conditions, restaurants, restrooms, children_playground, money_exchange, atm, nearest_hotel, attentions, bribes, security) VALUES {openairesponse}")
 
 send_telegram_message(
     user_name='shakhrillo',
-    message=openairesponse
+    message='Done'
 )
