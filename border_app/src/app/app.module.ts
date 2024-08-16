5fe2c4e3c16a65ba247faae11e03dc7ca3f50aef
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-08-16 15:12:05 +0500

    feat: Add ngx-leaflet library for interactive maps

--- border_app/src/app/app.module.ts
+++ border_app/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LeafletModule,
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+    // provideHttpClient(withInterceptorsFor([{
+    //   provide: HTTP_INTERCEPTORS,
+    //   // useClass: () => {},
+    //   multi: true
+    // }])),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+function withInterceptorsFor(arg0: { provide: import("@angular/core").InjectionToken<readonly import("@angular/common/http").HttpInterceptor[]>; useClass: any; multi: boolean; }[]): import("@angular/common/http").HttpFeature<import("@angular/common/http").HttpFeatureKind>
+{
+  throw new Error('Function not implemented.');
+}
+
