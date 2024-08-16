c6126f55cd90570418de2fd86fa169150a868442
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-08-16 14:48:33 +0500

    chore: Update project files and configurations

--- border_app/src/app/app.component.ts
+++ border_app/src/app/app.component.ts
@@ -1,10 +1,107 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import * as L from "leaflet";
+import * as geojson from "geojson";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'border_app';
+export class AppComponent implements AfterViewInit {
+
+  private map: L.Map | undefined;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [46.2276, 2.2137],
+      zoom: 5
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: 'Open Street Map'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  //  this.addBorders();
+  }
+  
+  data = [
+    {
+      "id": 1,
+      "name": "Mont Blanc Tunnel",
+      "country_from": "France",
+      "country_to": "Italy",
+      "coordinates": {
+        "lat": 45.8326,
+        "long": 6.8652
+      }
+    },
+    {
+      "id": 2,
+      "name": "Frejus Tunnel",
+      "country_from": "France",
+      "country_to": "Italy",
+      "coordinates": {
+        "lat": 45.1452,
+        "long": 6.7222
+      }
+    },
+    {
+      "id": 3,
+      "name": "Fernetti Border Crossing",
+      "country_from": "Italy",
+      "country_to": "Slovenia",
+      "coordinates": {
+        "lat": 45.6848,
+        "long": 13.8347
+      }
+    },
+    {
+      "id": 4,
+      "name": "Obrežje Border Crossing",
+      "country_from": "Slovenia",
+      "country_to": "Croatia",
+      "coordinates": {
+        "lat": 45.8331,
+        "long": 15.6774
+      }
+    },
+    {
+      "id": 5,
+      "name": "Bajakovo Border Crossing",
+      "country_from": "Croatia",
+      "country_to": "Serbia",
+      "coordinates": {
+        "lat": 45.0755,
+        "long": 19.1314
+      }
+    },
+    {
+      "id": 6,
+      "name": "Gradina Border Crossing",
+      "country_from": "Serbia",
+      "country_to": "Bulgaria",
+      "coordinates": {
+        "lat": 43.1347,
+        "long": 22.6483
+      }
+    },
+    {
+      "id": 7,
+      "name": "Kapitan Andreevo Border Crossing",
+      "country_from": "Bulgaria",
+      "country_to": "Turkey",
+      "coordinates": {
+        "lat": 41.7178,
+        "long": 26.3221
+      }
+    }
+  ]
+  
 }
