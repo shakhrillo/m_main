5af3617d9a61739b15675170ff166efa33d1a3d5
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-08-22 11:06:56 +0500

    chore: Update Leaflet styles and dependencies

--- FOLIUM_BACK/app.py
+++ FOLIUM_BACK/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, json, render_template_string
-
 import folium
 import googlemaps
+from geopy.distance import geodesic
+from shapely.geometry import Point
+import shapely.geometry as sg
+from shapely.geometry import box
+import geopandas as gpd
+from shapely.geometry import Polygon
+
+# with open("data/countries.json") as f:
+#     countries = json.load(f)
 
 app = Flask(__name__)
 
@@ -22,49 +30,23 @@ def fullscreen():
     m = folium.Map()
     return m.get_root().render()
 
-
-@app.route("/iframe")
-def iframe():
-    """Embed a map as an iframe on a page."""
-    m = folium.Map()
-
-    # set the iframe width and height
-    m.get_root().width = "800px"
-    m.get_root().height = "600px"
-    iframe = m.get_root()._repr_html_()
-
-    return render_template_string(
-        """
-            <!DOCTYPE html>
-            <html>
-                <head></head>
-                <body>
-                    <h1>Using an iframe</h1>
-                    {{ iframe|safe }}
-                </body>
-            </html>
-        """,
-        iframe=iframe,
-    )
-
+# def is_within_bounding_box(lat, lon, bbox):
+#     polygon = box(*bbox)  # Create a bounding box polygon
+#     point = Point(lon, lat)  # Create a point with (lon, lat) coordinates
+#     return point.within(polygon)
 
 @app.route("/components")
 def components():
-    """Extract map components and put those on a page."""
     m = folium.Map(
         width=800,
         height=600,
     )
 
-    paris = [48.8566, 2.3522]
     moscow = [55.7558, 37.6176]
-    japan = [35.6895, 139.6917]
-    south_korea = [37.5665, 126.9780]
-    pakistan = [30.3753, 69.3451]
-    islamabad = [33.6844, 73.0479]
-    hochiminh = [10.8107, 106.7501]
+    paris = moscow
     cape_town = [-33.9249, 18.4241]
 
+    crossed_countries = []
 
     # Request directions
     directions_result = gmaps.directions(
@@ -74,12 +56,8 @@ def components():
 
     if directions_result:
         route_polyline = directions_result[0]["overview_polyline"]["points"]
-        
-        # Decode the polyline
         decoded_route = googlemaps.convert.decode_polyline(route_polyline)
-        
         points = []
-        # # print("Decoded route:", decoded_route[0])
         for coord in decoded_route:
             points.append(
                 (coord['lat'], coord['lng'])
@@ -91,22 +69,50 @@ def components():
             weight=5,
             opacity=0.8,
         ).add_to(m)
+
+        print(len(points))
+        # print(points)
+        # radius = 100
+
+        polygon = Polygon(points)
+
+        path_ = "/Users/shakhrillo/Desktop/m_main/FOLIUM_BACK/data/110m_cultural/ne_110m_admin_0_countries.shp"
+        world = gpd.read_file(path_)
+
+        # print(world.columns)
+
+        # world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+        polygon_gdf = gpd.GeoDataFrame({'geometry': [polygon]}, crs="EPSG:4326")
+
+        intersections = world[world.intersects(polygon_gdf.unary_union)]
+
+        # Inspect the results
+        print(intersections)
+        # print(intersections.columns) 
+
+        # save points json
+        # with open("data/points.json", "w") as f:
+        #     json.dump(points, f)
         
-        # Check if decoded_route is a list of tuples with length 2
-        # if all(isinstance(coord, tuple) and len(coord) == 2 for coord in decoded_route):
-        #     folium.PolyLine(
-        #         locations=decoded_route,
-        #         color="blue",
-        #         weight=5,
-        #         opacity=0.8,
-        #     ).add_to(m)
+        # for point in points:
+        #     latitude_to_check, longitude_to_check = point
             
-        #     print("Decoded route is in the correct format")
-        # else:
-        #     print("Decoded route is not in the correct format")
-        #     print("Route format check:", [(type(coord), len(coord)) for coord in decoded_route])
+        #     for country in countries:
+        #         coordinates = country["coordinates"]
+                
+        #         if is_within_bounding_box(latitude_to_check, longitude_to_check, coordinates):
+        #             if country["name"] not in crossed_countries:
+        #                 crossed_countries.append(country["name"])
+        #                 break
+                
     else:
         print("No routes found")
+
+    print(f"Crossed countries: {crossed_countries}")
+    print(len(crossed_countries))
+    # extend the bounds of the map
+    m.fit_bounds([paris, cape_town])
 
     m.get_root().render()
     header = m.get_root().header.render()
