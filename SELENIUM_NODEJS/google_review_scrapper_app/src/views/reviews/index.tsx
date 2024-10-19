2505324a1d6afc8eab9186ad423c945daea47c73
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-10-19 12:06:34 +0500

    Refactor ReviewsView component to include place information and add delete functionality

--- SELENIUM_NODEJS/google_review_scrapper_app/src/views/reviews/index.tsx
+++ SELENIUM_NODEJS/google_review_scrapper_app/src/views/reviews/index.tsx
@@ -1,4 +1,4 @@
-import { collection, onSnapshot } from "firebase/firestore"
+import { collection, doc, onSnapshot } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { useAppSelector } from "../../app/hooks"
@@ -8,11 +8,13 @@ import { Modal, Carousel } from "react-bootstrap"
 
 import StarRating from "../../components/star-rating"
 import "../../style/view.css"
+import { getStorage, ref, getDownloadURL } from "firebase/storage"
 
 function ReviewsView() {
   let { place } = useParams()
   const [selectedImages, setSelectedImages] = useState([] as any[])
   const [show, setShow] = useState(false)
+  const [placeInfo, setPlaceInfo] = useState({} as any)
 
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
@@ -25,13 +27,32 @@ function ReviewsView() {
   useEffect(() => {
     if (!db || !place) return
     setReviews([])
+    const docReview = doc(db, "reviews", place)
+
+    const unsubscribe = onSnapshot(docReview, (doc) => {
+      if (doc.exists()) {
+        console.log("Document data:", doc.data())
+        setPlaceInfo({
+          ...doc.data(),
+          id: doc.id,
+        })
+      }
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [db, place])
+  
+  useEffect(() => {
+    if (!db || !place) return
+    setReviews([])
     const collectionReviews = collection(db, "reviews", place, "reviews")
 
-    onSnapshot(collectionReviews, (querySnapshot) => {
+    const unsubscribe = onSnapshot(collectionReviews, (querySnapshot) => {
       setReviews([]);
       querySnapshot.forEach((doc) => {
         const data = doc.data();
-        console.log('data', data);
         setReviews((prev) => [
           ...prev,
           {
@@ -41,6 +62,10 @@ function ReviewsView() {
         ])
       })
     })
+
+    return () => {
+      unsubscribe()
+    }
   }, [db, place])
 
   // Pagination calculations
@@ -62,6 +87,20 @@ function ReviewsView() {
       { icon: "bi-chat-square-text", text: "Content" },
       { icon: "bi-chat-text", text: "Response" },
     ],
+  }
+
+  const downloadFile = async (url: string) => {
+    const storage = getStorage()
+    const fileRef = ref(storage, url)
+    const fileUrl = await getDownloadURL(fileRef)
+
+    window.open(fileUrl, "_blank")
+  }
+
+  async function deleteReview(id: string) {
+    await fetch(`http://localhost:3000/reviews/${id}`, {
+      method: "DELETE",
+    })
   }
 
   return (
@@ -89,30 +128,59 @@ function ReviewsView() {
           </Button>
         </Modal.Footer>
       </Modal>
-      {/* <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>
-            All images
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {
-            selectedImages.map((imageSrc: string, imageId: number) => (
-              <img
-                key={imageId}
-                src={imageSrc}
-                style={{ width: "100%", marginBottom: "15px" }}
-              />
-            ))
-          }
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal> */}
       <div className="container">
+        {
+          placeInfo.info ? (
+
+            <div className="card mb-2">
+              <div className="card-body">
+                <h5 className="card-title">
+                  {placeInfo.info.mainTitle}
+                  <span className="badge bg-primary ms-2">
+                    {placeInfo.status}
+                  </span>
+                </h5>
+                <p className="card-text">{placeInfo.info.address.name} ({placeInfo.info.address.latitude} - {placeInfo.info.address.longitude})</p>
+                <p className="card-text">
+                  Created at: {placeInfo.createdAt}
+                </p>
+                <p className="card-text">
+                  Completed at: {placeInfo.completedAt}
+                </p>
+
+                <div className="view__rating">
+                  <StarRating rating={String(placeInfo.info.mainRate)} />
+                  <span>{placeInfo.info.mainReview} reviews</span>
+                </div>
+              </div>
+              <div className="card-footer">
+                <div className="d-flex gap-2">
+                  <button
+                    onClick={() => downloadFile(placeInfo.fileUrl)}
+                    className="btn btn-secondary"
+                  >
+                    <i className="bi-download me-2"></i>
+                    JSON
+                  </button>
+                  <button
+                    onClick={() => downloadFile(placeInfo.fileUrlCsv)}
+                    className="btn btn-secondary"
+                  >
+                    <i className="bi-download me-2"></i>
+                    CSV
+                  </button>
+                  <button
+                    onClick={() => deleteReview(placeInfo.id)}
+                    className="btn btn-danger ms-auto"
+                  >
+                    <i className="bi-trash me-2"></i>
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          ) : null
+        }
         <div className="card">
           <div className="card-body">
             <table className="table view__table">
