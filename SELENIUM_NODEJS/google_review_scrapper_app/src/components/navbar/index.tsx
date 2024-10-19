f4b466aea12c0b9c4efada569f8eceda19e81dc6
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-10-20 01:15:28 +0500

    Refactor navbar styles to update brand link and remove unnecessary background color

--- SELENIUM_NODEJS/google_review_scrapper_app/src/components/navbar/index.tsx
+++ SELENIUM_NODEJS/google_review_scrapper_app/src/components/navbar/index.tsx
@@ -1,9 +1,9 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { setUser } from "../../features/auth";
 import { signOutAction } from "../../features/auth/action";
 import { initFirebase } from "../../features/firebase/actions";
-import { setUser } from "../../features/auth";
 import "../../style/navbar.css";
 
 function Navbar() {
@@ -29,46 +29,23 @@ function Navbar() {
     dispatch(initFirebase());
   }, [])
 
+  function handleClick() {
+    if (user) {
+      dispatch(signOutAction());
+    } else {
+      window.location.href = '/auth';
+    }
+  }
+
   return (
     <nav className="navbar custom-navbar">
       <div className="container-fluid">
         <a className="navbar-brand" href={'/'}>
           <img src="https://cdn.freebiesupply.com/images/large/2x/amazon-logo-transparent.png" alt="" height="24" />
         </a>
-        <ul className="d-flex flex-row gap-3 navbar-nav align-items-center">
-          <li className="nav-item">
-            <a className="nav-link" href={'/about'}>About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href={'/price'}>Pricing</a>
-          </li>
-          <li className="nav-item">
-            <span>|</span>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" aria-disabled="true" href={'/dashboard'}>
-              <span className="badge bg-warning rounded-0">
-                Dashboard
-              </span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href={'/user'}>User</a>
-          </li>
-          <li className="nav-item">
-            {
-              user ? (
-                <a className="nav-link" onClick={() => {
-                  dispatch(signOutAction())
-                }}>Log out</a>
-              ) : (
-                <a className="nav-link" href={'/auth'}>
-                  Log in
-                </a>
-              )
-            }
-          </li>
-        </ul>
+        <button className="btn btn-outline-secondary me-2" type="button" onClick={handleClick}>
+          {user ? 'Log out' : 'Log in'}
+        </button>
       </div>
     </nav>
   )
