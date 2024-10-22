ceeaacd1f4cc59484dfe769d0df29948c4c0b903
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-10-22 15:27:57 +0500

    Refactor project structure and remove unused files

---
+++ SELENIUM_NODEJS/gcf-reviews/index.js
@@ -0,0 +1,19 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+exports.upperCase = onRequest((req, res) => {
+  logger.info("Request body", req.body);
+  res.send(req.body.text.toUpperCase());
+});
