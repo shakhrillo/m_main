39bdcda547b091e61900807343effcba26ee94ba
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-10-31 02:41:15 +0500

    Refactor Selenium configuration to use Chrome instead of Edge, update dependencies

--- SELENIUM_NODEJS/gen1/src/controllers/scraperController.js
+++ SELENIUM_NODEJS/gen1/src/controllers/scraperController.js
@@ -4,9 +4,37 @@ const { clickReviewTab, scrollAndCollectElements } = require('../utils/elementUt
 const filterUniqueElements = require('../utils/filter');
 const { waitTime } = require('../config/settings');
 
-async function main(url) {
+const Firestore = require('@google-cloud/firestore');
+
+const firestore = new Firestore({
+  projectId: 'map-review-scrap'
+});
+
+async function batchWriteLargeArray(collectionRef, data) {
+  const batch = firestore.batch();
+  const chunkSize = 500;
+
+  for (let i = 0; i < data.length; i += chunkSize) {
+    const chunk = data.slice(i, i + chunkSize);
+    chunk.forEach((doc) => {
+      const docRef = collectionRef.doc(doc.id);
+      batch.set(docRef, doc);
+    });
+
+    await batch.commit();
+  }
+}
+
+async function main(url, uid, pushId) {
   const browser = await launchBrowser();
   const page = await openPage(browser, url);
+  
+  await firestore.doc(`users/${uid}/reviews/${pushId}`).update({
+    title: await page.title(),
+    createdAt: new Date(),
+    status: 'in-progress'
+  });
+
   await wait(waitTime);
   await clickReviewTab(page);
   await wait(waitTime);
@@ -20,6 +48,24 @@ async function main(url) {
   }
 
   await browser.close();
+
+  let collectionReviews = firestore.collection(`users/${uid}/reviews/${pushId}/reviews`);
+  const messages = allElements.map((element) => {
+    return {
+      id: element.id || '',
+      text: element.text || '',
+      textContent: element.textContent || '',
+    };
+  });
+  await batchWriteLargeArray(collectionReviews, messages);
+
+  await firestore.doc(`users/${uid}/reviews/${pushId}`).update({
+    status: 'completed',
+    totalReviews: uniqueElements.length,
+    completedAt: new Date()
+  });
+
+  return uniqueElements;
 }
 
 // Export the main function for Google Cloud Functions
