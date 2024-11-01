641745532f33c6367b8150f0e71d242e1dbf6861
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-11-02 02:23:19 +0500

    Refactor CORS configuration to allow all origins and methods

--- GOOGLE/MAP_REVIEW_SCRAP/src/app.js
+++ GOOGLE/MAP_REVIEW_SCRAP/src/app.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+  credentials: true, // Allow credentials
+  optionsSuccessStatus: 204 // Some legacy browsers choke on 204
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
