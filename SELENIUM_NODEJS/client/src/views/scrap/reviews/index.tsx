feaec3f93e83973022821f240628c40cd5394992
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-11-03 14:52:02 +0500

    Refactor API endpoint URL in ScrapReviewsView

--- SELENIUM_NODEJS/client/src/views/scrap/reviews/index.tsx
+++ SELENIUM_NODEJS/client/src/views/scrap/reviews/index.tsx
@@ -39,7 +39,7 @@ function ScrapReviewsView() {
     const token = await user?.getIdToken()
 
     // const url = `http://34.122.24.195`;
-    await fetch("http://127.0.0.1:1337", {
+    await fetch("http://34.122.24.195", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
