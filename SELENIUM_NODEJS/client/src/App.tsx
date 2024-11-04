a2870133fff98e7a5afb7164f45f53fb5c87da34
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-11-04 14:18:22 +0500

    Refactor App.tsx to remove unused import and comments

--- SELENIUM_NODEJS/client/src/App.tsx
+++ SELENIUM_NODEJS/client/src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React from 'react';
 import AppRoutes from './AppRoutes';
 
--- SELENIUM_NODEJS/client/src/main.tsx
+++ SELENIUM_NODEJS/client/src/main.tsx
@@ -1,81 +1,9 @@
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
+import App from "./App"
 import { store } from "./app/store"
 import { FirebaseProvider } from "./contexts/FirebaseProvider"
 import "./index.scss"
-import AuthView from "./views/auth"
-import DashboardView from "./views/dashboard"
-import PaymentsHistoryView from "./views/payments/history"
-import PaymentsSubscriptionView from "./views/payments/subscription"
-import PricingView from "./views/pricing"
-import ScrapPlacesView from "./views/scrap/places"
-import ScrapReviewsView from "./views/scrap/reviews"
-import ScrapReviewsReviewView from "./views/scrap/reviews/review"
-import UsersView from "./views/user"
-import App from "./App"
-
-
-const router = createBrowserRouter([
-  {
-    path: "/auth",
-    element: <AuthView />,
-  },
-  {
-    path: "/ss",
-    element: <DashboardView />,
-    children: [
-      {
-        path: "scrap",
-        element: <Outlet />,
-        children: [
-          {
-            path: "reviews",
-            element: <ScrapReviewsView />,
-          },
-          {
-            path: "review/:place",
-            element: <ScrapReviewsReviewView />,
-          },
-          {
-            path: "places",
-            element: <ScrapPlacesView />
-          }
-        ]
-      },
-      {
-        path: "payments",
-        element: <Outlet />,
-        children: [
-          {
-            path: "subscription",
-            element: <PaymentsSubscriptionView />,
-          },
-          {
-            path: "history",
-            element: <PaymentsHistoryView />,
-          },
-        ]
-      }
-    ]
-  },
-  {
-    path: "/user",
-    element: <UsersView />,
-  },
-  {
-    path: "/about",
-    element: <div>About page</div>,
-  },
-  {
-    path: "/price",
-    element: <PricingView />,
-  },
-  {
-    path: "/contact",
-    element: <div>Contact page</div>,
-  }
-])
 
 const container = document.getElementById("root")
 
@@ -86,7 +14,6 @@ if (container) {
     <Provider store={store}>
       <FirebaseProvider>
         <App />
-        {/* <RouterProvider router={router} /> */}
       </FirebaseProvider>
     </Provider>,
   )
