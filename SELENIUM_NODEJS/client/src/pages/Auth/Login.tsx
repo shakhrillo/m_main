43d0711ba857672990ab594388d39f4051295a37
Mukhammad Tukhsanov <muxammadhotamo@gmail.com>
2024-11-05 14:41:05 +0500

    Login page updates

--- SELENIUM_NODEJS/client/src/pages/Auth/Login.tsx
+++ SELENIUM_NODEJS/client/src/pages/Auth/Login.tsx
@@ -39,64 +39,64 @@ const Login: React.FC = () => {
   }
 
   return (
-    <div className="login container-fluid d-flex justify-content-center align-items-center position-relative">
-      <nav className="login__navbar navbar navbar-expand-lg px-5 position-absolute">
-        <a className="login__navbar__logo navbar-brand" href="#">
+    <div className="login container-fluid">
+      <nav className="login__navbar">
+        <a href="#">
           <img
-            className="login__navbar__logo"
+            className="login__navbar-logo"
             src={logo}
             alt="GeoScraper logo"
           />
         </a>
       </nav>
-      <div className="login__card card">
-        <div className="card-body px-5 py-5">
-          <div className="login__card-header">
-            <h3 className="login__card-header-title">Login</h3>
-            <span className="login__text">
-              Enter your details to get sign in to your account
-            </span>
+      <div className="login__card">
+        <div className="login__card__header">
+          <h3 className="login__card-header-title">Login</h3>
+          <span className="login__text">
+            Enter your details to get sign in to your account
+          </span>
+        </div>
+        <form className="login__card__form" onSubmit={handleLogin}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="staticEmail"
+              placeholder="Enter email"
+              // value="email@example.com"
+            />
           </div>
-          <form onSubmit={handleLogin}>
-            <div className="form-group mb-2">
-              <input
-                type="text"
-                className="login__card-input form-control"
-                id="staticEmail"
-                placeholder="Enter email"
-                // value="email@example.com"
-              />
-            </div>
-            <div className="form-group mb-2">
-              <input
-                type="password"
-                className="login__card-input form-control"
-                placeholder="Password"
-              />
-            </div>
-            <button className="login__card-button w-100 mt-4 btn" type="submit">
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+          </div>
+          <div className="login__card__form-submit">
+            <button className="btn btn-primary" type="submit">
               Sign In
             </button>
-          </form>
-          <div className="text-center my-3 d-flex flex-column gap-3">
-            <span className="login__text">Or Sign in with</span>
-            <button
-              className="login__card-button btn border d-flex gap-2 justify-content-center"
-              type="button"
-              onClick={handleGoogleLogin}
-            >
-              <i className="bi bi-google"></i>
-              Login with Google
-            </button>
           </div>
-          <div className="login__text text-center">
-            <span>
-              Don't have an account?{" "}
-              <Link className="login__card-link" to="/auth/register">
-                Register
-              </Link>
-            </span>
-          </div>
+        </form>
+        <div className="login__card__footer">
+          <span className="login__text">Or Sign in with</span>
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            onClick={handleGoogleLogin}
+          >
+            <i className="bi bi-google"></i>
+            Login with Google
+          </button>
+        </div>
+        <div className="login__text">
+          <span>
+            Don't have an account?{" "}
+            <Link className="login__card__link" to="/auth/register">
+              Register
+            </Link>
+          </span>
         </div>
       </div>
       {/* <span className="text-nowrap mx-5">
