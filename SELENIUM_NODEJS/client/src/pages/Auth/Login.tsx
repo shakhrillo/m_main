3439ab2d496fbb4ab206855aab7672cb62bbd53f
Mukhammad Tukhsanov <muxammadhotamo@gmail.com>
2024-11-05 14:08:23 +0500

    Login page updates

--- SELENIUM_NODEJS/client/src/pages/Auth/Login.tsx
+++ SELENIUM_NODEJS/client/src/pages/Auth/Login.tsx
@@ -40,79 +40,69 @@ const Login: React.FC = () => {
 
   return (
     <div className="login container-fluid d-flex justify-content-center align-items-center position-relative">
-      <div className="row">
-        <nav className="login__navbar navbar navbar-expand-lg px-5 position-absolute">
-          <a className="navbar-brand" href="#">
-            <img src={logo} alt="GeoScraper logo" />
-          </a>
-        </nav>
-        <div className="col col-md-6 offset-md-3">
-          <div className="login__card card">
-            <div className="card-body px-5 py-5">
-              <div className="login__card--header">
-                <h3>Login</h3>
-                <span>Enter your details to get sign in to your account</span>
-              </div>
-              {/* <div className="text-center my-4">
-                <img src={logo} alt="logo" />
-              </div> */}
-              <form onSubmit={handleLogin}>
-                <div className="form-group mb-2">
-                  {/* <div className="mb-3 row"> */}
-                  {/* <label className="col-sm-3 col-form-label">Email</label> */}
-                  {/* <div className="col-sm-9"> */}
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="staticEmail"
-                    placeholder="Enter email"
-                    // value="email@example.com"
-                  />
-                  {/* </div> */}
-                </div>
-                <div className="form-group mb-2">
-                  {/* <div className="mb-3 row"> */}
-                  {/* <label className="col-sm-3 col-form-label">Password</label> */}
-                  {/* <div className="col-sm-9"> */}
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Password"
-                  />
-                  {/* </div> */}
-                </div>
-                <div className="d-grid gap-2 mt-4">
-                  <button className="btn" type="submit">
-                    Sign In
-                  </button>
-                </div>
-              </form>
-              {/* <hr /> */}
-              <div className="text-center my-3 d-flex flex-column gap-3">
-                <span>Or Sign in with</span>
-                <button
-                  className="btn border d-flex gap-2 justify-content-center"
-                  type="button"
-                  onClick={handleGoogleLogin}
-                >
-                  <i className="bi bi-google"></i>
-                  Login with Google
-                </button>
-              </div>
-              <div className="text-center">
-                <span>
-                  Don't have an account?{" "}
-                  <Link to="/auth/register">Register</Link>
-                </span>
-              </div>
-            </div>
+      <nav className="login__navbar navbar navbar-expand-lg px-5 position-absolute">
+        <a className="login__navbar__logo navbar-brand" href="#">
+          <img
+            className="login__navbar__logo"
+            src={logo}
+            alt="GeoScraper logo"
+          />
+        </a>
+      </nav>
+      <div className="login__card card">
+        <div className="card-body px-5 py-5">
+          <div className="login__card-header">
+            <h3 className="login__card-header-title">Login</h3>
+            <span className="login__text">
+              Enter your details to get sign in to your account
+            </span>
           </div>
-          <span className="text-nowrap mx-5">
-            Copeyright <i className="bi bi-c-circle"></i> GeoScraper 2024 |
-            Privacy Policy
-          </span>
+          <form onSubmit={handleLogin}>
+            <div className="form-group mb-2">
+              <input
+                type="text"
+                className="login__card-input form-control"
+                id="staticEmail"
+                placeholder="Enter email"
+                // value="email@example.com"
+              />
+            </div>
+            <div className="form-group mb-2">
+              <input
+                type="password"
+                className="login__card-input form-control"
+                placeholder="Password"
+              />
+            </div>
+            <button className="login__card-button w-100 mt-4 btn" type="submit">
+              Sign In
+            </button>
+          </form>
+          <div className="text-center my-3 d-flex flex-column gap-3">
+            <span className="login__text">Or Sign in with</span>
+            <button
+              className="login__card-button btn border d-flex gap-2 justify-content-center"
+              type="button"
+              onClick={handleGoogleLogin}
+            >
+              <i className="bi bi-google"></i>
+              Login with Google
+            </button>
+          </div>
+          <div className="login__text text-center">
+            <span>
+              Don't have an account?{" "}
+              <Link className="login__card-link" to="/auth/register">
+                Register
+              </Link>
+            </span>
+          </div>
         </div>
       </div>
+      {/* <span className="text-nowrap mx-5">
+        Copeyright <i className="bi bi-c-circle"></i> GeoScraper 2024 | Privacy
+        Policy
+      </span> */}
     </div>
   )
 }
