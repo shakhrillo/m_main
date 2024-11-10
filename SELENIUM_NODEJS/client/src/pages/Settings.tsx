aadf4ae36f4a7ebd218a89070ee4e0e56e52bcd4
Mukhammad Tukhsanov <muxammadhotamo@gmail.com>
2024-11-10 19:42:52 +0500

    Settings page changes and added font family Inter, _gridd files

--- SELENIUM_NODEJS/client/src/pages/Settings.tsx
+++ SELENIUM_NODEJS/client/src/pages/Settings.tsx
@@ -10,66 +10,49 @@ const Settings: React.FC = () => {
 
   return (
     <div>
-      <h2>
-        Settings
-      </h2>
+      <h2>Settings</h2>
       <div className="card">
-        <h3>
-          Account
-        </h3>
-        <p>
-          Real-time information and activities of your property
-        </p>
+        <h3>Account</h3>
+        <p>Real-time information and activities of your property</p>
 
-        <form action="">
-          <label htmlFor="language">
-            Language
-          </label>
-          <select
-            name="language"
-            id="language"
-            value={language}
-            onChange={(e) => setLanguage(e.target.value)}
-          >
-            <option value="en">
-              English
-            </option>
-            <option value="fr">
-              French
-            </option>
-            <option value="es">
-              Spanish
-            </option>
-            <option value="de">
-              German
-            </option>
-          </select>
-
-          <label htmlFor="notIsCompleted">
-            Notify when scraping is completed
-          </label>
-          <input
-            name="notIsCompleted"
-            type="checkbox"
-            checked={notIsCompleted}
-            onChange={() => setNotIsComplated(!notIsCompleted)}
-          />
-
-          <label htmlFor="notOnLogin">
-            Notify on login
-          </label>
-          <input
-            name="notOnLogin"
-            type="checkbox"
-            checked={notOnLogin}
-            onChange={() => setNotOnLogin(!notOnLogin)}
-          />
-
-          <br />
-
-          <button className="primary">
-            Save
-          </button>
+        <form action="" className="d-flex flex-column gap-2">
+          <div>
+            <label htmlFor="language">Language</label>
+            <select
+              name="language"
+              id="language"
+              value={language}
+              onChange={e => setLanguage(e.target.value)}
+            >
+              <option value="en">English</option>
+              <option value="fr">French</option>
+              <option value="es">Spanish</option>
+              <option value="de">German</option>
+            </select>
+          </div>
+          <div className="d-flex flex-column gap-1">
+            <div className="d-flex gap-1">
+              <input
+                name="notIsCompleted"
+                type="checkbox"
+                checked={notIsCompleted}
+                onChange={() => setNotIsComplated(!notIsCompleted)}
+              />
+              <label htmlFor="notIsCompleted">
+                Notify when scraping is completed
+              </label>
+            </div>
+            <div className="d-flex gap-1">
+              <input
+                name="notOnLogin"
+                type="checkbox"
+                checked={notOnLogin}
+                onChange={() => setNotOnLogin(!notOnLogin)}
+              />
+              <label htmlFor="notOnLogin">Notify on login</label>
+            </div>
+          </div>
+          <button className="primary">Save</button>
         </form>
       </div>
     </div>
