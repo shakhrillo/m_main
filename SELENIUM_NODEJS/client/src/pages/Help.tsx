2592071490566561937ac8b402d341b427fa7f8b
Mukhammad Tukhsanov <muxammadhotamo@gmail.com>
2024-11-10 21:13:13 +0500

    new chnages

--- SELENIUM_NODEJS/client/src/pages/Help.tsx
+++ SELENIUM_NODEJS/client/src/pages/Help.tsx
@@ -8,26 +8,24 @@ const Help: React.FC = () => {
     <div>
       <h2>Help</h2>
       <div className="card">
-        <h3>
-          FAQ
-        </h3>
-        <p>
-          Frequently asked questions
-        </p>
-        
-        <h4>
-          How to scrape reviews?
-        </h4>
-        <p>
-          To scrape reviews, go to the Scrap page and enter the URL of the place you want to scrape reviews from.
-        </p>
-
-        <h4>
-          How to download reviews?
-        </h4>
-        <p>
-          To download reviews, go to the Scrap page and click the download button.
-        </p>
+        <div>
+          <h3>FAQ</h3>
+          <p>Frequently asked questions</p>
+        </div>
+        <div className="mt-2">
+          <h4>How to scrape reviews?</h4>
+          <p>
+            To scrape reviews, go to the Scrap page and enter the URL of the
+            place you want to scrape reviews from.
+          </p>
+        </div>
+        <div className="mt-1">
+          <h4>How to download reviews?</h4>
+          <p>
+            To download reviews, go to the Scrap page and click the download
+            button.
+          </p>
+        </div>
       </div>
     </div>
   )
