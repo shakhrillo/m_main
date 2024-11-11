22ca70442523ec8aa9a1214894a8ef1ebbde8fd9
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-11-11 14:55:06 +0500

    Refactor extractReviewText function to improve text extraction logic; update logging for better error handling; add new CSV and JSON files for review data

--- GOOGLE/MAP_REVIEW_SCRAP/src/utils/extractReviewText.js
+++ GOOGLE/MAP_REVIEW_SCRAP/src/utils/extractReviewText.js
@@ -1,33 +1,45 @@
-const logger = require('./logger');
+const logger = require("./logger");
 
 /**
  * Extracts the text of a review from a review element.
- * 
+ *
  * @param {ElementHandle} element - The review element from which to extract the text.
  * @returns {Promise<string>} - A promise that resolves to the review text or an empty string if no review text is found.
  */
 async function extractReviewText(element) {
-  let reviewTextContent = '';
+  let reviewTextContent = "";
 
   try {
     // Locate the review container
-    const reviewContainer = await element.$$('.MyEned');
+    const reviewContainer = await element.$$(".MyEned");
     if (!reviewContainer.length) {
-      logger.warn('No review containers found in the provided element.');
+      logger.warn("No review containers found in the provided element.");
       return reviewTextContent; // Return empty if no review containers are found
     }
 
     // Extract the review text from the first review container
-    const reviewText = await reviewContainer[0].$('span');
-    if (!reviewText) {
-      logger.warn('No review text element found in the review container.');
-      return reviewTextContent; // Return empty if no review text element is found
+    const reviewContainerParent = await reviewContainer[0].evaluate(
+      (el) => el.parentElement
+    );
+
+    if (!reviewContainerParent) {
+      logger.warn("Parent element not found.");
+      return reviewTextContent;
     }
+    // const reviewText = await reviewContainer[0].$('span');
+    // if (!reviewText) {
+    //   logger.warn('No review text element found in the review container.');
+    //   return reviewTextContent; // Return empty if no review text element is found
+    // }
 
     // Retrieve the text content of the review
-    reviewTextContent = await reviewText.evaluate(el => el.textContent.trim());
+    // reviewTextContent = await reviewText.evaluate(el => el.textContent.trim());
+    reviewTextContent = await reviewContainerParent.evaluate((el) =>
+      el.textContent.trim()
+    );
+    console.log("Review text:", reviewTextContent || "No review text found");
   } catch (error) {
-    logger.error('Error extracting review text:', error);
+    logger.error("Error extracting review text:", error);
   }
 
   return reviewTextContent;
