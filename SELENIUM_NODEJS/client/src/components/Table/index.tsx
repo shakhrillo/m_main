20592f50d5604df1671cf98d15d49c7b0e97d4b4
Mukhammad Tukhsanov <muxammadhotamo@gmail.com>
2024-11-11 14:56:29 +0500

    scrap review table

--- SELENIUM_NODEJS/client/src/components/Table/index.tsx
+++ SELENIUM_NODEJS/client/src/components/Table/index.tsx
@@ -1,12 +1,13 @@
+import { an } from "vitest/dist/chunks/reporters.C4ZHgdxQ.js"
 import { TableHeader } from "./TableHeader"
 import { TableBody } from "./Tablebody"
 
-export const Table: React.FC<{ tableHeader: any; body: any[] }> = ({
-  tableHeader,
-  body,
-}) => {
+export const Table: React.FC<{
+  tableHeader: any
+  body: any[]
+}> = ({ tableHeader, body }) => {
   return (
-    <table>
+    <table className={"table"}>
       <TableHeader header={tableHeader} />
       <TableBody body={body} columns={tableHeader} />
     </table>
