540735d7068f7361dc2dba0644ea1dc149dca204
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-11-20 10:19:56 +0500

    Refactor main.js and scraperRoutes.js to integrate dotenv for environment variables, update Firestore collection paths, and enhance Docker container management

--- GOOGLE/MAP_REVIEW_SCRAP/machines/main.js
+++ GOOGLE/MAP_REVIEW_SCRAP/machines/main.js
@@ -1,20 +1,6 @@
-const fs = require("fs");
+require("dotenv").config();
 const { launchBrowser, openPage } = require("./services/browser");
-const {
-  waitTitle,
-  openReviewTab,
-  getInitialReviews,
-  watchNewReviews,
-  highLightElement,
-  sortReviews,
-  handleElementActions,
-  elementReviewComment,
-  elementReviewQA,
-  copyReviewURL,
-} = require("./services/page");
-const { Subject, concatMap, interval, take, map, defer } = require("rxjs");
 const { firestore } = require("./services/firebase");
-const newNodes$ = new Subject();
 
 async function wait(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -43,20 +29,29 @@ async function extractAriaLabel(page, selector) {
 
 const { uploadFile } = require("./services/storage");
 
-async function init({ url, userId, reviewId, limit, sortBy }) {
+async function init() {
+  const url = process.env.URL;
+  const userId = process.env.USER_ID;
+  const reviewId = process.env.REVIEW_ID;
+  const limit = process.env.LIMIT;
+  const sortBy = process.env.SORT_BY;
+
   console.log("Setting up...");
-  await firestore.collection(`users/${userId}/reviews`).doc(reviewId).set(
-    {
-      url,
-      userId,
-      reviewId,
-      limit,
-      sortBy,
-      createdAt: new Date(),
-      pending: true,
-    },
-    { merge: true }
-  );
+  await firestore
+    .collection(`users/${userId}/reviewOverview`)
+    .doc(reviewId)
+    .set(
+      {
+        url,
+        userId,
+        reviewId,
+        limit,
+        sortBy,
+        createdAt: new Date(),
+        pending: true,
+      },
+      { merge: true }
+    );
   console.log("Initializing...");
   const browser = await launchBrowser();
   const page = await openPage(browser, url);
@@ -120,89 +115,17 @@ async function init({ url, userId, reviewId, limit, sortBy }) {
   await page.close();
   await browser.close();
   await firestore
-    .collection(`users/${userId}/reviews`)
+    .collection(`users/${userId}/reviewOverview`)
     .doc(reviewId)
     .set(
       {
         ...data,
         pending: false,
+        completedAt: new Date(),
       },
       { merge: true }
     );
   console.log("Done");
-  // close docker container
-
-  // page.exposeFunction("newNodes", function (record) {
-  //   newNodes$.next(record);
-  // });
-  // console.log(`Scraping reviews for ${url}...`);
-
-  // const title = await waitTitle(page);
-  // console.log("Title:", title);
-
-  // await firestore.collection(`users/${userId}/reviews`).doc(reviewId).set(
-  //   {
-  //     title,
-  //     updatedAt: new Date(),
-  //   },
-  //   { merge: true }
-  // );
-
-  // let count = 0;
-
-  // newNodes$
-  //   .pipe(
-  //     concatMap((record) =>
-  //       interval(2000).pipe(
-  //         take(1),
-  //         map(() => record)
-  //       )
-  //     )
-  //   )
-  //   .subscribe(async (record) => {
-  //     console.log(`Checked ${count} reviews`);
-  //     await highLightElement(page, record);
-  //     await handleElementActions(page, record);
-  //     // await new Promise((resolve) => setTimeout(resolve, 500));
-  //     const reviewComment = await elementReviewComment(page, record);
-  //     console.log("Review Comment:", reviewComment);
-  //     const reviewQA = await elementReviewQA(page, record);
-  //     console.log("Review QA:", reviewQA);
-
-  //     if (
-  //       reviewComment.includes("More") ||
-  //       reviewQA.find((qa) => qa.includes("More"))
-  //     ) {
-  //       console.error("More...");
-  //       await new Promise((resolve) => setTimeout(resolve, 50000));
-  //     }
-
-  //     count++;
-  //   });
-
-  // await new Promise((resolve) => setTimeout(resolve, 2000));
-  // console.log("Opening review tab...");
-  // await openReviewTab(page);
-  // await new Promise((resolve) => setTimeout(resolve, 2000));
-  // console.log("Sorting reviews...");
-  // await sortReviews(page, sortBy);
-
-  // console.log("Getting initial reviews...");
-  // const initialReviews = await getInitialReviews(page);
-  // initialReviews.forEach((review) => newNodes$.next(review));
-
-  // watchNewReviews(page);
-
-  // await page.close();
-  // await browser.close();
 }
 
-init({
-  url: "https://maps.app.goo.gl/mccerYhUvrcA8tdCA",
-  reviewId: "xJuwdTi7mbb5lPWbj5sF",
-  userId: "83gDir7H21dnNXyk06BvGHN13v72",
-  limit: 50000,
-  sortBy: "Newest",
-  iat: 1732062432,
-  exp: 1732105632,
-});
+init();
--- GOOGLE/MAP_REVIEW_SCRAP/src/routes/scraperRoutes.js
+++ GOOGLE/MAP_REVIEW_SCRAP/src/routes/scraperRoutes.js
@@ -1,32 +1,17 @@
+require("dotenv").config();
+const FIREBASE_KEY_BASE64 = process.env.FIREBASE_KEY_BASE64;
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
-const scraperController = require("../controllers/scraperController");
-const { scrapePageData } = require("../controllers/scrapePageDataController");
-const {
-  runDocker,
-  removeDocker,
-  generateDockerConfig,
-} = require("../controllers/dockerController");
 const logger = require("../config/logger");
-const scrapePageComments = require("../controllers/scrapePageCommentsController");
-
-async function executeScraping(containerName, port, controller, req, res) {
-  try {
-    const data = await runDocker(
-      containerName,
-      port,
-      async () => await controller(req.data, port, containerName)
-    );
-    await removeDocker(containerName);
-
-    res.json(data || { message: "Scraping started" });
-  } catch (error) {
-    logger.error(`Error in ${controller.name} route:`, error.message);
-    await removeDocker(containerName);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-}
+const { createEnvironment } = require("../controllers/environmentController");
+const {
+  buildImage,
+  listContainers,
+  removeContainer,
+  startContainer,
+} = require("../controllers/dockerController");
+const { createReview } = require("../controllers/reviewController");
 
 router.post("/", authMiddleware, async (req, res) => {
   // save new file
@@ -37,10 +22,47 @@ router.post("/", authMiddleware, async (req, res) => {
   // executeScraping(containerName, port, scraperController, req, res);
 });
 
-router.post("/info", authMiddleware, (req, res) => {
-  res.json({ message: "Scraping info" });
-  // const { port, containerName } = generateDockerConfig();
-  // executeScraping(containerName, port, scrapePageData, req, res);
+router.post("/info", authMiddleware, async (req, res) => {
+  try {
+    const { url, userId, reviewId, limit, sortBy } = req.data;
+
+    const sanitizedUserId = userId.toLowerCase().replace(/[^a-z0-9_-]/g, "");
+    const sanitizedReviewId = reviewId
+      .toLowerCase()
+      .replace(/[^a-z0-9_-]/g, "");
+
+    createEnvironment(
+      `FIREBASE_KEY_BASE64=${FIREBASE_KEY_BASE64} 
+      STORAGE_BUCKET=${process.env.STORAGE_BUCKET}
+      URL=${url} 
+      USER_ID=${sanitizedUserId} 
+      REVIEW_ID=${sanitizedReviewId} 
+      LIMIT=${limit} 
+      SORT_BY=${sortBy}`
+    );
+
+    // Build Docker image
+    const buildTag = `r_${sanitizedUserId}_${sanitizedReviewId}`;
+    console.log(`Building Docker image with tag: ${buildTag}`);
+    await buildImage(buildTag);
+    const lists = await listContainers();
+    await Promise.all(
+      lists.map(async (containerId) => {
+        await removeContainer(containerId);
+      })
+    );
+    const containerName = `r_${sanitizedUserId}_${sanitizedReviewId}`;
+    const container = await startContainer(containerName, buildTag);
+
+    await createReview(userId, {
+      url,
+      ...container,
+    });
+    res.json({ message: "Container started", ...container });
+  } catch (error) {
+    logger.error(`Error: ${error.message}`);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 module.exports = router;
