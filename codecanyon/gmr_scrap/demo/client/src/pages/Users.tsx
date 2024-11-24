f25921dcf78f381a4594fa85516f2c25a1195509
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-11-24 09:12:26 +0500

    Refactor Users page to use a table layout for improved user display and readability

--- codecanyon/gmr_scrap/demo/client/src/pages/Users.tsx
+++ codecanyon/gmr_scrap/demo/client/src/pages/Users.tsx
@@ -35,13 +35,29 @@ const Users: React.FC = () => {
   return (
     <div>
       <h2>Users</h2>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>
-            {user.displayName} - {user.email} - {user.coin}
-          </li>
-        ))}
-      </ul>
+      <div className="card">
+        <div className="card-body">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Display Name</th>
+                <th>Email</th>
+                <th>Coin</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {users.map(user => (
+                <tr key={user.id}>
+                  <td>{user.displayName}</td>
+                  <td>{user.email}</td>
+                  <td>{user.coin || "0"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
