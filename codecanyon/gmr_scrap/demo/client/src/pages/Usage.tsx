1d7a1ccb3ee66ec18bf0be5b80bdaae504a5c3f4
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-07 20:19:03 +0500

    Refactor Usage component to display CPU and memory usage; update Firestore document path and remove unused code for improved clarity and performance.

--- codecanyon/gmr_scrap/demo/client/src/pages/Usage.tsx
+++ codecanyon/gmr_scrap/demo/client/src/pages/Usage.tsx
@@ -2,86 +2,63 @@ import { doc, onSnapshot, updateDoc } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import { useFirebase } from "../contexts/FirebaseProvider"
 
-interface IUsage {
-  Active: string
-  Reclaimable: string
-  Size: string
-  TotalCount: string
-  Type: string
-}
-
 const Usage: React.FC = () => {
   const { firestore } = useFirebase()
-  const [usage, setUsage] = useState<IUsage[]>([])
+  const [totalCpuUsage, setTotalCpuUsage] = useState(0)
+  const [totalMemory, setTotalMemory] = useState(0)
+  const [memoryFree, setMemoryFree] = useState(0)
+  const [memoryUsed, setMemoryUsed] = useState(0)
 
   useEffect(() => {
     if (!firestore) return
-    const docAppInfo = doc(firestore, "app", "usage")
+    const docAppInfo = doc(firestore, "docker", "info")
 
     const unsubscribe = onSnapshot(docAppInfo, snapshot => {
       const data = snapshot.data()
-      if (!data || !data.usage) return
-      const dataArray = data.usage.split("\n").filter((item: any) => item)
-      const objects = dataArray.map((item: any) => JSON.parse(item))
-      console.log(objects)
-      setUsage(objects)
+      if (!data || !data.info) return
+      console.log(JSON.parse(data.info))
+      const info = JSON.parse(data.info)
+
+      // const totalCpuUsage = info.NCPU;  // Total number of CPUs
+      // const totalMemory = info.MemTotal;  // Total memory in bytes
+      // const memoryFree = info.MemFree;  // Free memory in bytes
+      // const memoryUsed = totalMemory - memoryFree;  // Used memory
+
+      // console.log(`Total CPUs: ${totalCpuUsage}`);
+      // console.log(`Total Memory: ${totalMemory / (1024 * 1024)} MB`);
+      // console.log(`Used Memory: ${memoryUsed / (1024 * 1024)} MB`);
+      // console.log(`Free Memory: ${memoryFree / (1024 * 1024)} MB`);
+      if (info) {
+        setTotalCpuUsage(info.NCPU)
+        setTotalMemory(info.MemTotal / (1024 * 1024))
+        setMemoryFree(info.MemFree / (1024 * 1024))
+        setMemoryUsed((info.MemTotal - info.MemFree) / (1024 * 1024))
+      }
     })
 
     return () => unsubscribe()
   }, [firestore])
 
-  async function refresh() {
-    const docAppInfo = doc(firestore, "app", "usage")
-    await updateDoc(docAppInfo, {
-      refresh: true,
-    })
-  }
-
-  async function clearCache() {
-    const docAppInfo = doc(firestore, "app", "usage")
-    await updateDoc(docAppInfo, {
-      clearCache: true,
-    })
-  }
-
   return (
     <div>
       <h2>Usage</h2>
-      <div className="card">
-        <div className="card-header">
-          <button className="button" onClick={clearCache}>
-            Clear cache
-          </button>
-          <button className="button button-primary" onClick={refresh}>
-            Refresh
-          </button>
-        </div>
-        <div className="card-body">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Type</th>
-                <th>Size</th>
-                <th>Active</th>
-                <th>Total Count</th>
-                <th>Reclaimable</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {usage.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.Type}</td>
-                  <td>{item.Size}</td>
-                  <td>{item.Active}</td>
-                  <td>{item.TotalCount}</td>
-                  <td>{item.Reclaimable}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>CPU</th>
+            <th>Memory</th>
+            <th>All containers</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{totalCpuUsage}</td>
+            <td>{totalMemory}</td>
+            <td>{memoryUsed}</td>
+            <td>{memoryFree}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
