b84af417c06bd8b99b1b48d951e4d364253ab0cf
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-07 16:13:28 +0500

    Add stats logging to watchMachine function; store machine stats in Firestore if available.

--- codecanyon/gmr_scrap/demo/functions/src/machines/watchMachine.js
+++ codecanyon/gmr_scrap/demo/functions/src/machines/watchMachine.js
@@ -4,9 +4,20 @@ const watchMachine = async (event) => {
   const machineId = event.params.machineId;
   const previousDocument = event.data.before.data();
   const document = event.data.after.data();
+  const stats = document.stats;
 
   const userId = document.userId;
   const reviewId = document.reviewId;
+
+  if (stats) {
+    await admin
+      .firestore()
+      .collection(`machines/${machineId}/stats`)
+      .add({
+        ...document.stats,
+        createdAt: new Date(),
+      });
+  }
 
   if (machineId.includes("info")) {
     await admin
