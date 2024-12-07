b3c8f6ed9677cfe819f6ad0a025f9d050b7fff22
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-07 22:09:06 +0500

    Enhance Firebase initialization with environment variable checks and error handling; add support for Firestore emulator configuration in development mode.

--- codecanyon/gmr_scrap/demo/server/firebase.js
+++ codecanyon/gmr_scrap/demo/server/firebase.js
@@ -1,16 +1,51 @@
-// firebase.js
 const admin = require("firebase-admin");
+const fs = require("fs");
+const path = require("path");
 
-// Initialize Firebase Admin SDK
-admin.initializeApp({
-  projectId: "fir-scrapp",
-});
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
 
-// Connect Firestore to the Emulator
+const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
+if (!FIREBASE_PROJECT_ID) {
+  throw new Error("FIREBASE_PROJECT_ID not found in environment variables");
+}
+
+const serviceAccountPath = path.resolve(__dirname, "../firebase.json");
+if (!fs.existsSync(serviceAccountPath)) {
+  throw new Error("firebase.json not found");
+}
+
+const firebasekeysPath = path.resolve(__dirname, "../firebasekeys.json");
+if (
+  process.env.NODE_ENV !== "development" &&
+  !fs.existsSync(firebasekeysPath)
+) {
+  throw new Error("firebasekeys.json not found");
+}
+
+admin.initializeApp(
+  process.env.NODE_ENV === "development"
+    ? { projectId: FIREBASE_PROJECT_ID }
+    : {
+        credential: admin.credential.cert(firebasekeysPath),
+        storageBucket: process.env.STORAGE_BUCKET,
+        projectId: FIREBASE_PROJECT_ID,
+      }
+);
+
 const db = admin.firestore();
-db.settings({
-  host: "localhost:9100", // Firestore emulator host
-  ssl: false, // Disable SSL for emulator
-});
+
+if (process.env.NODE_ENV === "development") {
+  const serviceAccountJson = JSON.parse(
+    fs.readFileSync(serviceAccountPath, "utf8")
+  );
+  const firestorePort = serviceAccountJson.emulators?.firestore?.port;
+  if (firestorePort) {
+    db.settings({ host: `localhost:${firestorePort}`, ssl: false });
+  } else {
+    console.warn("Firestore emulator port not found in firebase.json");
+  }
+}
 
 module.exports = { admin, db };
