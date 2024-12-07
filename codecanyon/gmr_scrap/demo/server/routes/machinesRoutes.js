0ef807ae9bd99041597a11ef1f8701ecacb9dd86
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-07 21:02:50 +0500

    Remove unused Docker-related routes and functions from machinesRoutes; simplify cache clearing endpoint.

--- codecanyon/gmr_scrap/demo/server/routes/machinesRoutes.js
+++ codecanyon/gmr_scrap/demo/server/routes/machinesRoutes.js
@@ -2,20 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
-const {
-  removeImage,
-  removeUnusedImages,
-  removeContainerByName,
-} = require("../controllers/dockerController");
 
 router.post("/clear-cache", authMiddleware, async (req, res) => {
-  await removeUnusedImages();
   res.json({ message: "Cache cleared" });
 });
 
 router.delete("/", authMiddleware, async (req, res) => {
-  const { buildTag } = req.data;
-  await removeContainerByName(buildTag);
   res.json({ message: "Image removed" });
 });
 
