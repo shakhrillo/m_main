d80fd15739398374cd5d85215a47c3b449c9bad2
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-08 11:38:56 +0500

    Refactor Docker event handling: streamline status management, improve stream subscription logic, and enhance error handling for Docker info updates.

--- codecanyon/gmr_scrap/demo/server/docker.js
+++ codecanyon/gmr_scrap/demo/server/docker.js
@@ -12,9 +12,6 @@ async function watchEvents() {
   eventsStream.setEncoding("utf8");
   eventsStream.on("data", (data) => {
     const str = data.toString();
-    let status = str.match(/"status":"([^"]+)"/);
-    status = status ? status[1] : "";
-
     const overviewPrefix = process.env.MACHINES_OVERVIEW_PREFIX || "info";
     const reviewsPrefix = process.env.MACHINES_REVIEWS_PREFIX || "comments";
 
@@ -25,24 +22,19 @@ async function watchEvents() {
         const lines = str.trim().split("\n");
         const parsedData = lines.map((line) => JSON.parse(line));
         for (const data of parsedData) {
+          let status = data.status;
           const name = data.Actor.Attributes.name;
           updateMachine(name, data);
 
           if (status !== "destroy") {
-            if (activeStreams.has(name)) {
-              console.log("---".repeat(100));
-              console.log(`Container: ${name} is already being watched`);
-              console.log("---".repeat(100));
-              const stream = activeStreams.get(name);
-              stream.removeAllListeners(); // Unsubscribe from the stream
-              activeStreams.delete(name); // Remove from active streams
-              console.log(`Unsubscribed from container: ${name}`);
-            }
-
             const container = docker.getContainer(name);
+
             if (status === "die") {
               container.remove();
-            } else if (container) {
+              return;
+            }
+
+            if (!activeStreams.has(name)) {
               container.stats((err, stream) => {
                 if (err) {
                   console.error(err);
@@ -51,29 +43,31 @@ async function watchEvents() {
                 stream.setEncoding("utf8");
                 stream.on("data", (data) => {
                   const stats = JSON.parse(data);
-                  // console.log("Stats:", stats);
                   updateMachine(name, { stats });
                 });
-
-                activeStreams.set(name, stream); // Keep track of the stream
+                activeStreams.set(name, stream);
               });
             }
-          }
-        }
-
-        if (status === "destroy") {
-          docker.info(async (err, info) => {
-            if (err) {
-              console.error("Error fetching Docker info:", err);
-            } else {
-              console.log("Docker Engine Info:");
-              try {
-                updateDockerInfo(info);
-              } catch (error) {
-                console.error("Error saving Docker info:", error);
-              }
+          } else {
+            const stream = activeStreams.get(name);
+            if (stream) {
+              stream.destroy();
+              activeStreams.delete(name);
+              console.log("Stream destroyed for", name);
             }
-          });
+
+            docker.info(async (err, info) => {
+              if (err) {
+                console.error("Error fetching Docker info:", err);
+              } else {
+                try {
+                  updateDockerInfo(info);
+                } catch (error) {
+                  console.error("Error saving Docker info:", error);
+                }
+              }
+            });
+          }
         }
       } catch (error) {
         console.log("Error saving machine data:", error);
