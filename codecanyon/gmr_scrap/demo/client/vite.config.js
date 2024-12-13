44b611ee2a32495b09da8ce4aa2571f5643fa15c
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-13 14:30:32 +0500

    Add Vite configuration files: initialize vite.config.js and vite.config.d.ts for project setup

---
+++ codecanyon/gmr_scrap/demo/client/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        open: true,
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true,
+            },
+        },
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "src/setupTests",
+        mockReset: true,
+    },
+});
