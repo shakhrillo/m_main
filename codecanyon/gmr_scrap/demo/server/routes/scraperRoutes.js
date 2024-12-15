acc3b5520a136a4b1325f44b3087742515b8617e
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-15 17:33:49 +0500

    Add service account scopes in callInsert function

--- codecanyon/gmr_scrap/demo/server/routes/scraperRoutes.js
+++ codecanyon/gmr_scrap/demo/server/routes/scraperRoutes.js
@@ -159,14 +159,15 @@ async function callInsert(config) {
       serviceAccounts: [
         {
           email: "gmrscrap@map-review-scrap.iam.gserviceaccount.com",
-          // scopes: [
-          //   "https://www.googleapis.com/auth/devstorage.read_only",
-          //   "https://www.googleapis.com/auth/logging.write",
-          //   "https://www.googleapis.com/auth/monitoring.write",
-          //   "https://www.googleapis.com/auth/service.management.readonly",
-          //   "https://www.googleapis.com/auth/servicecontrol",
-          //   "https://www.googleapis.com/auth/trace.append",
-          // ],
+          scopes: [
+            "https://www.googleapis.com/auth/devstorage.read_only",
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring.write",
+            "https://www.googleapis.com/auth/service.management.readonly",
+            "https://www.googleapis.com/auth/servicecontrol",
+            "https://www.googleapis.com/auth/trace.append",
+            "https://www.googleapis.com/auth/cloud-platform",
+          ],
         },
       ],
       shieldedInstanceConfig: {
