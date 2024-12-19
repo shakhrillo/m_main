5ba8a62e1934bf2ee2ef6f7b786435f78a8a35bd
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-19 22:54:52 +0500

    Comment out Selenium server URL and update Chrome binary path in selenium.js

--- codecanyon/gmr_scrap/demo/selenium_machine/services/selenium.js
+++ codecanyon/gmr_scrap/demo/selenium_machine/services/selenium.js
@@ -5,11 +5,11 @@ async function getDriver() {
   const options = new chrome.Options();
   options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
   options.setLoggingPrefs({ browser: "ALL" });
-  // options.setChromeBinaryPath("/usr/bin/chromedriver");
+  options.setChromeBinaryPath("/usr/bin/chromedriver");
   options.excludeSwitches("enable-automation");
 
   const driver = await new Builder()
-    .usingServer("http://10.128.0.32:4444/wd/hub")
+    // .usingServer("http://10.128.0.32:4444/wd/hub")
     .setChromeOptions(options)
     .forBrowser(Browser.CHROME)
     .build();
