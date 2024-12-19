81e890be8ddbf5b207657f8d0ac8f0b885b854ae
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-19 13:09:35 +0500

    Comment out unused Google Cloud zone constant in configuration

--- codecanyon/gmr_scrap/demo/server/config/constants.js
+++ codecanyon/gmr_scrap/demo/server/config/constants.js
@@ -5,7 +5,7 @@ const googleCloudZones = [
   "northamerica-northeast2-a",
   "northamerica-northeast2-b",
   "northamerica-northeast2-c",
-  "us-central1-a",
+  // "us-central1-a",
   "us-central1-b",
   "us-central1-c",
   "us-central1-f",
