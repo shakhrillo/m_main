d4b77240952c38931509f74326c7bbe63fd680c7
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-19 15:18:22 +0500

    Set fixed zone to 'us-central1-f' in startVmInstance function

--- codecanyon/gmr_scrap/demo/server/controllers/googleController.js
+++ codecanyon/gmr_scrap/demo/server/controllers/googleController.js
@@ -10,8 +10,9 @@ const googleProjectId = process.env.GOOGLE_PROJECT_ID || "fir-scrapp";
 const serviceAccountEmail = process.env.SERVICE_ACCOUNT_EMAIL || "";
 
 async function startVmInstance(config) {
-  const zone =
-    googleCloudZones[Math.floor(Math.random() * googleCloudZones.length)];
+  // const zone =
+  //   googleCloudZones[Math.floor(Math.random() * googleCloudZones.length)];
+  const zone = "us-central1-f";
   const uniqueInstanceName = config.tag.replace(/_/g, "-");
   // let machineType = "e2-small";
   const diskSizeGb = 50;
