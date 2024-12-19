cb02b8f88cb7487326f6d5394b00ab7df5e83fad
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-19 22:57:23 +0500

    Update Chrome binary path in selenium.js to use stable version

--- codecanyon/gmr_scrap/demo/selenium_machine/services/selenium.js
+++ codecanyon/gmr_scrap/demo/selenium_machine/services/selenium.js
@@ -5,7 +5,7 @@ async function getDriver() {
   const options = new chrome.Options();
   options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
   options.setLoggingPrefs({ browser: "ALL" });
-  options.setChromeBinaryPath("/usr/bin/chromedriver");
+  options.setChromeBinaryPath("/usr/bin/google-chrome-stable");
   options.excludeSwitches("enable-automation");
 
   const driver = await new Builder()
