cf6d8c6d2355bd5f143a2841ccfd6eb2b4216799
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-21 10:49:14 +0500

    Remove unused menu context and related imports from Scrap component

--- codecanyon/gmr_scrap/demo/client/src/pages/Scrap.tsx
+++ codecanyon/gmr_scrap/demo/client/src/pages/Scrap.tsx
@@ -1,10 +1,7 @@
 import React from "react"
 import { ReviewsForm } from "../components/reviews-form"
-import { useMenu } from "../context/MenuContext/MenuContext"
-import menuIcon from "../assets/icons/list.svg"
 
 const Scrap: React.FC = () => {
-  const { toggleMenu } = useMenu()
   return (
     <div className="col">
       <div className="row">
