448f540cfec23f8f5fc8b5e2c9b80daae9a56e0b
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-30 16:39:17 +0500

    Refactor Firebase configuration: destructure environment variables for cleaner code and improved readability.

--- codecanyon/gmr_scrap/demo/client/src/firebaseConfig.tsx
+++ codecanyon/gmr_scrap/demo/client/src/firebaseConfig.tsx
@@ -1,43 +1,40 @@
 import { initializeApp } from "firebase/app"
 import { connectAuthEmulator, getAuth } from "firebase/auth"
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore"
-console.log("firebaseConfig.tsx")
-console.log(import.meta.env)
-const environment = import.meta.env.VITE_ENV
-const apiKey = import.meta.env.VITE_FIREBASE_API_KEY
-const authDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN
-const projectId = import.meta.env.VITE_FIREBASE_PROJECT_ID
-const storageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET
-const messagingSenderId = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID
-const appId = import.meta.env.VITE_FIREBASE_APP_ID
-const measurementId = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+
+const {
+  VITE_ENV,
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_ID,
+  VITE_FIREBASE_MEASUREMENT_ID,
+  VITE_FIREBASE_AUTH_EMULATOR_PORT,
+  VITE_FIRESTORE_EMULATOR_PORT,
+} = import.meta.env
 
 const firebaseConfig = {
-  apiKey,
-  authDomain,
-  projectId,
-  storageBucket,
-  messagingSenderId,
-  appId,
-  measurementId,
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID,
+  measurementId: VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth()
 const firestore = getFirestore()
 
-console.log("environment", environment)
-
-if (environment === "development") {
+if (VITE_ENV === "development") {
   connectAuthEmulator(
     auth,
-    `http://localhost:${import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_PORT}`,
+    `http://localhost:${VITE_FIREBASE_AUTH_EMULATOR_PORT}`,
   )
-  connectFirestoreEmulator(
-    firestore,
-    "localhost",
-    import.meta.env.VITE_FIRESTORE_EMULATOR_PORT,
-  )
+  connectFirestoreEmulator(firestore, "localhost", VITE_FIRESTORE_EMULATOR_PORT)
 }
 
 export { app, auth, firestore }
