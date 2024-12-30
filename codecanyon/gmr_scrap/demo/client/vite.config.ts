8f91d33515bc63d4f93f11114bb92eddccbbc36c
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-12-30 16:27:14 +0500

    Refactor Vite configuration and SCSS imports: replace @import with @use for better modularity, remove deprecated vite.dev.js and vite.config.js files, and update package.json scripts for improved development setup.

--- codecanyon/gmr_scrap/demo/client/vite.config.ts
+++ codecanyon/gmr_scrap/demo/client/vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
+    host: true,
     open: true,
   },
+  envDir: "../",
   css: {
     preprocessorOptions: {
       scss: {
         quietDeps: true,
+        api: "modern",
       },
     },
   },
