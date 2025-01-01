6332b6a1ff6be21ac36995ac7c987ea7e08f2ecd
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-01-01 22:44:42 +0500

    Enhance Sidebar component: update layout for better alignment, add new icons, and improve title and subtitle styling for clarity.

--- codecanyon/gmr_scrap/demo/client/src/layouts/DashboardLayout/Sidebar.tsx
+++ codecanyon/gmr_scrap/demo/client/src/layouts/DashboardLayout/Sidebar.tsx
@@ -5,6 +5,8 @@ import {
   IconCreditCard,
   IconSettings,
   IconUser,
+  IconChevronRight,
+  IconMessageSearch,
 } from "@tabler/icons-react";
 import { createElement, FC } from "react";
 
@@ -30,18 +32,23 @@ const SidebarItem: FC<NavItemProps> = ({ to, icon, label, badge }) => (
       `sidebar-item ${isActive ? "active" : ""}`.trim()
     }
   >
-    {createElement(icon, { size: 20, className: "me-3" })}
-    <span className="label">{label}</span>
+    {createElement(icon, { size: 20, className: "me-2" })}
+    <span className="label me-auto">{label}</span>
     {badge !== undefined && (
       <span className="badge text-bg-primary ms-auto">{badge}</span>
     )}
+    {createElement(IconChevronRight, { size: 16, className: "ms-2" })}
   </NavLink>
 );
 
 const Sidebar: FC = () => (
   <nav className="sidebar">
     <div className="sidebar-header">
-      <h5 className="text-primary">GMR Scrap</h5>
+      <h5 className="sidebar-title">
+        {/* <IconMessageSearch size={30} /> */}
+        GMRS
+      </h5>
+      <small className="sidebar-subtitle">Google Maps Review Scraper</small>
     </div>
     <div className="sidebar-menu">
       {navItems.map((item) => (
