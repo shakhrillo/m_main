ab945d66d28ef55cd4c1a52ddd897aa4b4da2c32
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-01-08 11:50:16 +0500

    Refactor Navbar component; move to components directory and update import paths for better organization

--- codecanyon/gmr_scrap/demo/client/src/layouts/DashboardLayout/DashboardLayout.tsx
+++ codecanyon/gmr_scrap/demo/client/src/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
+import Navbar from "../../components/Navbar";
 
 const DashboardLayout: React.FC = () => {
   return (
