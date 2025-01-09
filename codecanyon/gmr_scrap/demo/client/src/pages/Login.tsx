9a849d79b89e05b4505635b1aa2ad15643fcc957
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-01-09 16:13:32 +0500

    Refactor Login component; enhance layout and styling for improved user experience and accessibility

--- codecanyon/gmr_scrap/demo/client/src/pages/Login.tsx
+++ codecanyon/gmr_scrap/demo/client/src/pages/Login.tsx
@@ -2,6 +2,11 @@ import { User } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useOutletContext } from "react-router-dom";
 import { googleLogin, login } from "../services/authService";
+import {
+  IconAlertCircle,
+  IconBrandGoogle,
+  IconBrandMeta,
+} from "@tabler/icons-react";
 
 const Login: React.FC = () => {
   const user = useOutletContext<User>();
@@ -36,67 +41,90 @@ const Login: React.FC = () => {
   }
 
   return (
-    <div className="container">
+    <div className="container-fluid auth">
       <div className="row">
-        <div className="col col-md-6 col-lg-5 mx-auto">
-          <div className="card">
-            <div className="card-body">
-              <h1 className="card-title text-center">Sign In</h1>
-              <p className="text-center">Sign in to your account</p>
-              <hr />
-              <form onSubmit={handleLogin}>
-                <div className="mb-3">
-                  <label className="form-label" htmlFor="email">
-                    Email <span className="text-danger">*</span>
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Enter your email"
-                    className="form-control"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label className="form-label" htmlFor="password">
-                    Password <span className="text-danger">*</span>
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Enter your password"
-                    className="form-control"
-                  />
-                </div>
-                <button className="btn btn-primary w-100" type="submit">
-                  Sign In
-                </button>
-                {error && <div className="text-danger mt-3">{error}</div>}
-                <div className="mt-3 text-center">
-                  <Link to="/auth/reset-password" className="btn btn-link">
-                    Forgot password?
-                  </Link>
-                </div>
-              </form>
-              <hr />
-              <button
-                className="btn btn-outline-primary w-100 mb-3"
-                onClick={handleGoogleLogin}
-              >
-                <i className="bi bi-google me-2"></i>
-                Login with Google
+        <div className="col-md-4">
+          <div className="auth-content">
+            <h3>Google Map Reviews Scraping Tool</h3>
+            <p>Sign in to your account</p>
+            <form
+              onSubmit={handleLogin}
+              noValidate
+              className="needs-validation was-validated mb-3"
+            >
+              <div className="mb-3">
+                <label className="form-label" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email"
+                  className="form-control"
+                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                  required
+                />
+                <small className="invalid-feedback">
+                  <IconAlertCircle className="me-2" size={20} />
+                  Please enter a valid email address.
+                </small>
+              </div>
+              <div className="mb-3">
+                <label className="form-label" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Enter your password"
+                  className="form-control"
+                  pattern=".{6,}"
+                  required
+                />
+                <small className="invalid-feedback">
+                  <IconAlertCircle className="me-2" size={20} />
+                  Password must be at least 6 characters long.
+                </small>
+              </div>
+              <button className="btn btn-primary" type="submit">
+                Sign In
               </button>
-              <p className="text-center mb-0">
-                Don't have an account?{" "}
-                <Link to="/auth/register" className="text-primary">
-                  Register
-                </Link>
-              </p>
+            </form>
+            <p className="m-0">
+              Don't have an account? <br />
+              <Link to="/register">Create an account</Link>
+            </p>
+            <h3 className="mt-4">Other ways to sign in</h3>
+            <p>Sign in using your social media account.</p>
+
+            <div className="row g-3">
+              <div className="col">
+                <button
+                  className="btn btn-primary w-100"
+                  onClick={handleGoogleLogin}
+                >
+                  <IconBrandGoogle className="me-2" size={20} />
+                  Sign in with Google
+                </button>
+              </div>
+              <div className="col">
+                <button
+                  className="btn btn-primary w-100"
+                  onClick={handleGoogleLogin}
+                >
+                  <IconBrandMeta className="me-2" size={20} />
+                  Sign in with Meta
+                </button>
+              </div>
             </div>
           </div>
+        </div>
+        <div className="col-md-8">
+          <div className="auth-image"></div>
         </div>
       </div>
     </div>
