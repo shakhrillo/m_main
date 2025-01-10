720291360f580924f263d4b778d0ff0d41b61368
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-01-10 15:54:13 +0500

    Refactor SingleReview component; integrate ReviewInfo logic directly and remove unused component for improved code simplicity

--- codecanyon/gmr_scrap/demo/client/src/pages/SingleReview.tsx
+++ codecanyon/gmr_scrap/demo/client/src/pages/SingleReview.tsx
@@ -1,14 +1,60 @@
+import { User } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useOutletContext, useParams } from "react-router-dom";
 import ReviewComments from "../components/review-comments";
-import ReviewInfo from "../components/review-info";
+import { validateUrlData } from "../services/scrapService";
 
 function SingleReview() {
+  const { uid } = useOutletContext<User>();
+  const { place } = useParams() as { place: string };
+  const [info, setPlaceInfo] = useState<any>({});
+
+  useEffect(() => {
+    const sunbscription = validateUrlData(place, uid).subscribe((data) => {
+      setPlaceInfo(data);
+    });
+
+    return () => {
+      sunbscription.unsubscribe();
+    };
+  }, [place]);
+
   return (
     <div className="container-fluid">
       <div className="row row-cols-1 g-3">
         <div className="col">
           <div className="card">
-            <div className="card-body">
-              <ReviewInfo />
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img
+                  src={info.screenshot}
+                  className="img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">{info.title}</h5>
+                  <p className="card-text">{info.address}</p>
+                  <hr />
+                  <ul className="list-unstyled">
+                    <li>
+                      <strong>Status:</strong> {info.status}
+                    </li>
+                    <li>
+                      <strong>Average Rating:</strong>{" "}
+                      {info.rating ? info.rating : "N/A"}
+                    </li>
+                    <li>
+                      <strong>Extracted Reviews:</strong>{" "}
+                      {info.totalReviews || 0}
+                    </li>
+                    <li>
+                      <strong>Spent Time:</strong>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
