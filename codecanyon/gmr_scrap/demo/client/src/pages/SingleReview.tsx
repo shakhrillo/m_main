6cf9eeeeafaa1c8d84932de1a75f81bcba6f1a0e
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-01-10 16:46:21 +0500

    Remove ReviewImages, ReviewVideos, and ReviewComments components for improved code simplicity and maintainability; add react-player dependency

--- codecanyon/gmr_scrap/demo/client/src/pages/SingleReview.tsx
+++ codecanyon/gmr_scrap/demo/client/src/pages/SingleReview.tsx
@@ -1,21 +1,49 @@
 import { User } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { Tab, Table, Tabs } from "react-bootstrap";
 import { useOutletContext, useParams } from "react-router-dom";
-import ReviewComments from "../components/review-comments";
-import { validateUrlData } from "../services/scrapService";
+import {
+  scrapData,
+  scrapImages,
+  scrapVideos,
+  validateUrlData,
+} from "../services/scrapService";
+import { Gallery, Item } from "react-photoswipe-gallery";
+import ReactPlayer from "react-player";
 
 function SingleReview() {
   const { uid } = useOutletContext<User>();
   const { place } = useParams() as { place: string };
   const [info, setPlaceInfo] = useState<any>({});
+  const [reviews, setReviews] = useState<any[]>([]);
+  const [images, setImages] = useState<any[]>([]);
+  const [videos, setVideos] = useState<any[]>([]);
 
   useEffect(() => {
-    const sunbscription = validateUrlData(place, uid).subscribe((data) => {
-      setPlaceInfo(data);
+    const placeInfoSubbscription = validateUrlData(place, uid).subscribe(
+      (data) => {
+        setPlaceInfo(data);
+      },
+    );
+
+    const reviewsSubscription = scrapData(place, uid).subscribe((data) => {
+      console.log("data", data);
+      setReviews(data);
+    });
+
+    const imagesSubscription = scrapImages(place, uid).subscribe((data) => {
+      console.log("images", data);
+      setImages(data);
+    });
+
+    const videosSubscription = scrapVideos(place, uid).subscribe((data) => {
+      console.log("videos", data);
+      setVideos(data);
     });
 
     return () => {
-      sunbscription.unsubscribe();
+      placeInfoSubbscription.unsubscribe();
+      reviewsSubscription.unsubscribe();
     };
   }, [place]);
 
@@ -59,7 +87,92 @@ function SingleReview() {
           </div>
         </div>
         <div className="col">
-          <ReviewComments />
+          <div className="card">
+            <div className="card-body">
+              <Tabs
+                defaultActiveKey="comments"
+                id="scrap-tabs"
+                className="mb-3"
+              >
+                <Tab eventKey="comments" title="Comments">
+                  <Table striped bordered hover>
+                    <thead>
+                      <tr>
+                        <th>Date</th>
+                        <th>Rating</th>
+                        <th>Review</th>
+                        <th>QA</th>
+                        <th>Response</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {reviews.map((review) => (
+                        <tr key={review.id}>
+                          <td>{}</td>
+                          <td>{review.rating}</td>
+                          <td>{review.review}</td>
+                          <td>
+                            <ul>
+                              {/* {review..map((qa, index) => (
+                        <li key={index} className="list-group-item">
+                          <small className="text-nowrap">{qa}</small>
+                        </li>
+                      ))} */}
+                            </ul>
+                          </td>
+                          <td>{/* {review.response} */}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                </Tab>
+                <Tab eventKey="images" title="Images">
+                  <div className="row row-cols-6 g-2">
+                    <Gallery
+                      options={{
+                        zoom: false,
+                        showHideAnimationType: "none",
+                        hideAnimationDuration: 0,
+                        showAnimationDuration: 0,
+                        zoomAnimationDuration: 0,
+                        bgOpacity: 0.9,
+                      }}
+                    >
+                      {images.map((img, index) => (
+                        <Item original={img.thumb || img.videoUrl} key={index}>
+                          {({ ref, open }) => (
+                            <div className="col">
+                              <img
+                                ref={ref}
+                                onClick={open}
+                                src={img.thumb || img.videoUrl}
+                                alt={`Review ${index}`}
+                                className="img-fluid img-thumbnail rounded"
+                              />
+                            </div>
+                          )}
+                        </Item>
+                      ))}
+                    </Gallery>
+                  </div>
+                </Tab>
+                <Tab eventKey="videos" title="Videos">
+                  <div className="row row-cols-6 g-2">
+                    {videos.map((video, index) => (
+                      <div className="col" key={index}>
+                        <ReactPlayer
+                          url={video.videoUrl}
+                          controls
+                          width="100%"
+                          height="100%"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </Tab>
+              </Tabs>
+            </div>
+          </div>
         </div>
       </div>
     </div>
