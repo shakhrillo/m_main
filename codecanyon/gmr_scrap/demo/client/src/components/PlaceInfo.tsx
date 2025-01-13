19d3a7a013bd7717fbc3534a563c5553f2365c98
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-01-13 23:04:45 +0500

    Enhance PlaceInfo component with additional details and layout improvements, including new buttons for CSV and JSON downloads

--- codecanyon/gmr_scrap/demo/client/src/components/PlaceInfo.tsx
+++ codecanyon/gmr_scrap/demo/client/src/components/PlaceInfo.tsx
@@ -1,10 +1,10 @@
-import { Card, Row, Col, Stack } from "react-bootstrap";
-import { GoogleMap } from "./GoogleMap";
+import { Button, Card, Col, Row, Stack } from "react-bootstrap";
 import { IReview } from "../services/scrapService";
-import { Ratings } from "./Ratings";
 import formatNumber from "../utils/formatNumber";
-import { spentTime } from "../utils/spentTime";
 import { formatTimestamp } from "../utils/formatTimestamp";
+import { spentTime } from "../utils/spentTime";
+import { GoogleMap } from "./GoogleMap";
+import { Ratings } from "./Ratings";
 import { StatusInfo } from "./StatusInfo";
 
 export const PlaceInfo = ({ info }: { info: IReview | undefined }) => {
@@ -25,36 +25,70 @@ export const PlaceInfo = ({ info }: { info: IReview | undefined }) => {
           <Card.Body>
             <Card.Title className="text-primary">{info.title}</Card.Title>
             <Ratings info={info} />
-            <hr />
-            <Stack direction="horizontal" className="mb-2">
-              <span className="me-auto">Status:</span>{" "}
-              <StatusInfo info={info} />
-            </Stack>
-            <Stack direction="horizontal" className="mb-2">
-              <span className="me-auto">Comments:</span>{" "}
-              <strong>
-                {formatNumber(info.totalReviews)} reviews /{" "}
-                {formatNumber(info.totalOwnerReviews)} replies
-              </strong>
-            </Stack>
-            <Stack direction="horizontal" className="mb-2">
-              <span className="me-auto">Media:</span>
-              <strong>
-                {formatNumber(info.totalImages)} images /{" "}
-                {formatNumber(info.totalVideos)} videos
-              </strong>
-            </Stack>
-            <Stack direction="horizontal" className="mb-2">
-              <span className="me-auto">Spent Time:</span>
-              <strong>{spentTime(info)}</strong>
-            </Stack>
-            <Stack direction="horizontal" className="mb-2">
-              <span className="me-auto">Created At:</span>
-              <strong>
-                {formatTimestamp(info.createdAt).date}
-                {" | "}
-                {formatTimestamp(info.createdAt).time}
-              </strong>
+            <div className="my-3">
+              <Stack direction="horizontal" className="py-1">
+                <span className="me-auto">Status:</span>{" "}
+                <StatusInfo info={info} />
+              </Stack>
+              <Stack direction="horizontal" className="py-1">
+                <span className="me-auto">Limit:</span>
+                <strong>{info.limit} comments</strong>
+              </Stack>
+              <Stack direction="horizontal" className="py-1">
+                <span className="me-auto">Sort:</span>
+                <strong>{info.sortBy}</strong>
+              </Stack>
+              <Stack direction="horizontal" className="py-1">
+                <span className="me-auto">Extract Image Urls:</span>
+                <strong>{info.extractImageUrls ? "Yes" : "No"}</strong>
+              </Stack>
+              <Stack direction="horizontal" className="py-1">
+                <span className="me-auto">Extract Video Urls:</span>
+                <strong>{info.extractVideoUrls ? "Yes" : "No"}</strong>
+              </Stack>
+              <Stack direction="horizontal" className="py-1">
+                <span className="me-auto">Extract Owner Replies:</span>
+                <strong>{info.extractOwnerResponse ? "Yes" : "No"}</strong>
+              </Stack>
+              <Stack direction="horizontal" className="py-1">
+                <span className="me-auto">Coordinates:</span>{" "}
+                <strong>
+                  {info.location?.latitude}, {info.location?.longitude}
+                </strong>
+              </Stack>
+              <Stack direction="horizontal" className="py-1">
+                <span className="me-auto">Comments:</span>{" "}
+                <strong>
+                  {formatNumber(info.totalReviews)} reviews /{" "}
+                  {formatNumber(info.totalOwnerReviews)} replies
+                </strong>
+              </Stack>
+              <Stack direction="horizontal" className="py-1">
+                <span className="me-auto">Media:</span>
+                <strong>
+                  {formatNumber(info.totalImages)} images /{" "}
+                  {formatNumber(info.totalVideos)} videos
+                </strong>
+              </Stack>
+              <Stack direction="horizontal" className="py-1">
+                <span className="me-auto">Spent Time:</span>
+                <strong>{spentTime(info)}</strong>
+              </Stack>
+              <Stack direction="horizontal" className="py-1">
+                <span className="me-auto">Created At:</span>
+                <strong>
+                  {formatTimestamp(info.createdAt).date}{" "}
+                  {formatTimestamp(info.createdAt).time}
+                </strong>
+              </Stack>
+            </div>
+            <Stack gap={3} direction="horizontal">
+              <Button variant="secondary" className="w-100">
+                Download CSV
+              </Button>
+              <Button variant="outline-secondary" className="w-100">
+                Download JSON
+              </Button>
             </Stack>
           </Card.Body>
         </Col>
