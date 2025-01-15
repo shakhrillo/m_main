ed3ab00be9be17caa2b75fad4795f5f0b658a56c
Mukhammad Tukhsanov <muxammadhotamo@gmail.com>
2025-01-15 14:33:06 +0500

    Login Page changes

--- codecanyon/gmr_scrap/demo/client/src/pages/Login.tsx
+++ codecanyon/gmr_scrap/demo/client/src/pages/Login.tsx
@@ -4,10 +4,10 @@ import { Link, useNavigate, useOutletContext } from "react-router-dom";
 import { googleLogin, login } from "../services/authService";
 import {
   IconAlertCircle,
-  IconBrandGoogle,
+  IconBrandGoogleFilled,
   IconBrandMeta,
 } from "@tabler/icons-react";
-import logo from "../assets/logo.svg";
+import logo from "../assets/logo_1.svg";
 import {
   Button,
   Col,
@@ -34,6 +34,7 @@ export const Login: React.FC = () => {
 
   async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    setError(null);
 
     try {
       await login(email, password);
@@ -54,81 +55,124 @@ export const Login: React.FC = () => {
   return (
     <Container fluid>
       <Row>
-        <Col md={6}>
+        {/* Left Side: Authentication Section */}
+        <Col md={6} className="auth">
+          <Image src={logo} alt="Logo" width="200" className="auth-logo" />
           <div className="auth-content">
-            <Image src={logo} alt="_Logo" width="200" className="mb-5" />
-            <h3>Welcome!</h3>
-            <p>Sign in to your account</p>
-            <Form
-              onSubmit={handleLogin}
-              noValidate
-              className="needs-validation was-validated"
-            >
-              <FormGroup className="mb-3" controlId="email">
-                <Form.Label>Email</Form.Label>
-                <FormControl
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Enter your email"
-                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-                  required
-                  isInvalid={
-                    !email.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)
-                  }
-                />
-                <Form.Control.Feedback type={"invalid"}>
-                  <IconAlertCircle className="me-2" size={20} />
-                  Please enter a valid email address.
-                </Form.Control.Feedback>
-              </FormGroup>
-              <FormGroup className="mb-3" controlId="password">
-                <Form.Label>Password</Form.Label>
-                <FormControl
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="Enter your password"
-                  pattern=".{6,}"
-                  required
-                />
-                <Form.Control.Feedback type={"invalid"}>
-                  <IconAlertCircle className="me-2" size={20} />
-                  Password must be at least 6 characters long.
-                </Form.Control.Feedback>
-              </FormGroup>
+            <Form onSubmit={handleLogin} noValidate className="auth-form">
+              {/* Error Message */}
+              {error && (
+                <div className="alert alert-danger text-center" role="alert">
+                  {error}
+                </div>
+              )}
+
+              {/* Header */}
+              <div className="auth-header">
+                <div>
+                  <h2 className="auth-title">Get Started Now</h2>
+                  <p className="auth-subtitle">
+                    Enter your credentials to access your account.
+                  </p>
+                </div>
+
+                {/* Social Login Buttons */}
+                <Row className="g-3">
+                  <Col>
+                    <Button
+                      className="button-outline w-100"
+                      onClick={handleGoogleLogin}
+                    >
+                      <IconBrandGoogleFilled className="me-2" size={20} />
+                      Log in with Google
+                    </Button>
+                  </Col>
+                  <Col>
+                    <Button
+                      className="button-outline w-100"
+                      onClick={handleGoogleLogin}
+                    >
+                      <IconBrandMeta className="me-2" size={20} />
+                      Log in with Meta
+                    </Button>
+                  </Col>
+                </Row>
+              </div>
+
+              {/* Divider */}
+              <div className="text-center my-3 position-relative">
+                <hr />
+                <span className="auth-devider">or</span>
+              </div>
+
+              {/* Login Form */}
+              <div>
+                <FormGroup className="mb-3" controlId="email">
+                  <Form.Label className="input-label">Email</Form.Label>
+                  <FormControl
+                    type="email"
+                    className="input"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Enter your email"
+                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                    required
+                    isInvalid={
+                      !!email &&
+                      !/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(email)
+                    }
+                  />
+
+                  <Form.Control.Feedback type={"invalid"}>
+                    <IconAlertCircle className="me-2" size={20} />
+                    Please enter a valid email address.
+                  </Form.Control.Feedback>
+                </FormGroup>
+
+                <FormGroup className="mb-3" controlId="password">
+                  <Form.Label className="input-label">Password</Form.Label>
+                  <FormControl
+                    type="password"
+                    className="input"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Enter your password"
+                    pattern=".{6,}"
+                    required
+                  />
+                  <Form.Control.Feedback type={"invalid"}>
+                    <IconAlertCircle className="me-2" size={20} />
+                    Password must be at least 6 characters long.
+                  </Form.Control.Feedback>
+                  <div className="text-end">
+                    <Link to={"#"} className="auth-link">
+                      Forgot password?
+                    </Link>
+                  </div>
+                </FormGroup>
+              </div>
+
+              {/* Submit Button */}
               <Button variant="primary" className="w-100" type="submit">
-                Sign In
+                Login
               </Button>
+
+              {/* Register Link */}
+              <div className="text-center mt-3 auth-link">
+                Don't have an account?{" "}
+                <Link className="text-decoration-none" to="/register">
+                  Create an account
+                </Link>
+              </div>
             </Form>
-            <div className="text-center my-3">Or login with</div>
-            <Row className="g-3">
-              <Col>
-                <Button
-                  variant="primary"
-                  className="w-100"
-                  onClick={handleGoogleLogin}
-                >
-                  <IconBrandGoogle className="me-2" size={20} />
-                  Google
-                </Button>
-              </Col>
-              <Col>
-                <Button
-                  variant="primary"
-                  className="w-100"
-                  onClick={handleGoogleLogin}
-                >
-                  <IconBrandMeta className="me-2" size={20} />
-                  Meta
-                </Button>
-              </Col>
-            </Row>
-            <div className="text-center mt-3">
-              Don't have an account? <Link to="/register">Register</Link>
-            </div>
+          </div>
+          {/* Footer */}
+          <div className="auth-footer">
+            <p>2025 GMRS, All right Reserved</p>
           </div>
         </Col>
+
+        {/* Right Side: Illustration */}
         <Col md={6}>
           <div className="auth-image"></div>
         </Col>
