34e022e6f367a7e38f874a06ca739e0bf960deb4
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-01-15 17:25:33 +0500

    Refactor Login component layout and styles; update logo and improve structure for better responsiveness

--- codecanyon/gmr_scrap/demo/client/src/pages/Login.tsx
+++ codecanyon/gmr_scrap/demo/client/src/pages/Login.tsx
@@ -7,7 +7,7 @@ import {
   IconBrandGoogleFilled,
   IconBrandMeta,
 } from "@tabler/icons-react";
-import logo from "../assets/logo_1.svg";
+import logo from "../assets/logo.svg";
 import {
   Alert,
   Button,
@@ -57,103 +57,111 @@ export const Login: React.FC = () => {
   return (
     <Container fluid>
       <Row>
-        <Col md={6} className="auth">
-          <Image src={logo} alt="Logo" width="200" className="auth-logo" />
-          <Stack className="auth-content my-auto">
-            <Form onSubmit={handleLogin} noValidate className="auth-form">
-              <Stack gap={4}>
-                <Stack>
+        <Col md={6}>
+          <Container fluid>
+            <Row className="vh-100">
+              <Col md={12}>
+                <Image src={logo} alt="Logo" width="200" />
+              </Col>
+
+              <Row>
+                <Col md={8} className="mx-auto">
                   <h2>Get Started Now</h2>
                   <p>Enter your credentials to access your account.</p>
-                </Stack>
+                </Col>
+                <Col md={8} className="mx-auto">
+                  <Row className="g-3">
+                    <Col xl={6}>
+                      <Button className="w-100" onClick={handleGoogleLogin}>
+                        <IconBrandGoogleFilled className="me-2" size={20} />
+                        Log in with Google
+                      </Button>
+                    </Col>
+                    <Col xl={6}>
+                      <Button className="w-100" onClick={handleGoogleLogin}>
+                        <IconBrandMeta className="me-2" size={20} />
+                        Log in with Meta
+                      </Button>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col md={8} className="mx-auto">
+                  <div className="auth-divider my-3">or</div>
+                </Col>
+                <Col md={8} className="mx-auto">
+                  <Form>
+                    <FormGroup className="mb-3" controlId="email">
+                      <Form.Label>Email</Form.Label>
+                      <FormControl
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter your email"
+                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                        required
+                        isInvalid={
+                          !!email &&
+                          !/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(
+                            email,
+                          )
+                        }
+                      />
 
-                <Row className="g-3">
-                  <Col>
-                    <Button className="w-100" onClick={handleGoogleLogin}>
-                      <IconBrandGoogleFilled className="me-2" size={20} />
-                      Log in with Google
+                      <Form.Control.Feedback type={"invalid"}>
+                        <IconAlertCircle className="me-2" size={20} />
+                        Please enter a valid email address.
+                      </Form.Control.Feedback>
+                    </FormGroup>
+
+                    <FormGroup className="mb-3" controlId="password">
+                      <Form.Label>Password</Form.Label>
+                      <FormControl
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter your password"
+                        pattern=".{6,}"
+                        required
+                      />
+                      <Form.Control.Feedback type={"invalid"}>
+                        <IconAlertCircle className="me-2" size={20} />
+                        Password must be at least 6 characters long.
+                      </Form.Control.Feedback>
+                      <div className="text-end">
+                        <NavLink to="#">Forgot password?</NavLink>
+                      </div>
+                    </FormGroup>
+
+                    <Button variant="primary" className="w-100" type="submit">
+                      Login
                     </Button>
-                  </Col>
-                  <Col>
-                    <Button className="w-100" onClick={handleGoogleLogin}>
-                      <IconBrandMeta className="me-2" size={20} />
-                      Log in with Meta
-                    </Button>
-                  </Col>
-                </Row>
-              </Stack>
-              <div className="auth-divider my-3">or</div>
-              <Stack>
-                <FormGroup className="mb-3" controlId="email">
-                  <Form.Label>Email</Form.Label>
-                  <FormControl
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Enter your email"
-                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-                    required
-                    isInvalid={
-                      !!email &&
-                      !/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(email)
-                    }
-                  />
-
-                  <Form.Control.Feedback type={"invalid"}>
-                    <IconAlertCircle className="me-2" size={20} />
-                    Please enter a valid email address.
-                  </Form.Control.Feedback>
-                </FormGroup>
-
-                <FormGroup className="mb-3" controlId="password">
-                  <Form.Label>Password</Form.Label>
-                  <FormControl
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Enter your password"
-                    pattern=".{6,}"
-                    required
-                  />
-                  <Form.Control.Feedback type={"invalid"}>
-                    <IconAlertCircle className="me-2" size={20} />
-                    Password must be at least 6 characters long.
-                  </Form.Control.Feedback>
-                  <div className="text-end">
-                    <NavLink to="#">Forgot password?</NavLink>
-                  </div>
-                </FormGroup>
-              </Stack>
-
-              <Button variant="primary" className="w-100" type="submit">
-                Login
-              </Button>
-              {error && (
-                <Alert variant="danger" role="alert">
-                  {error}
-                </Alert>
-              )}
-
-              <Stack
-                direction="horizontal"
-                gap={1}
-                className="mx-auto mt-3 auth-link"
-              >
-                Don't have an account?{" "}
-                <NavLink className="text-decoration-none" to="/register">
-                  Create an account
-                </NavLink>
-              </Stack>
-            </Form>
-          </Stack>
-          <Stack
-            direction="horizontal"
-            className="flex flex-none justify-content-center"
-          >
-            <p className="text-center text-sm text-gray-600">
-              2025 GMRS, All rights Reserved
-            </p>
-          </Stack>
+                    {error && (
+                      <Alert variant="danger" role="alert">
+                        {error}
+                      </Alert>
+                    )}
+                  </Form>
+                </Col>
+                <Col md={8} className="mx-auto">
+                  <Stack
+                    direction="horizontal"
+                    gap={1}
+                    className="mx-auto mt-3 auth-link"
+                  >
+                    Don't have an account?{" "}
+                    <NavLink className="text-decoration-none" to="/register">
+                      Create an account
+                    </NavLink>
+                  </Stack>
+                </Col>
+              </Row>
+              <Col md={12}>
+                <p className="text-center text-sm text-gray-600">
+                  2025 GMRS, All rights Reserved
+                </p>
+              </Col>
+            </Row>
+          </Container>
         </Col>
 
         <Col md={6} className="d-none d-md-block">
