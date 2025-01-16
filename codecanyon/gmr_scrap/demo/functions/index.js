f59f0e344f39703fe03e7d261de5ae72ab191eb8
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-01-17 00:56:13 +0500

    Add Firebase export metadata and update Dockerfile for data integration

--- codecanyon/gmr_scrap/demo/functions/index.js
+++ codecanyon/gmr_scrap/demo/functions/index.js
@@ -1,4 +1,9 @@
-require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config({
+  path: path.join(__dirname, "../.env"),
+});
+
 const functions = require("firebase-functions/v1");
 const admin = require("firebase-admin");
 const {
@@ -13,9 +18,6 @@ const processReviewCreated = require("./src/containers/processReviewCreated");
 const processContainerWritten = require("./src/containers/processContainerWritten");
 const userTopUp = require("./src/user/userTopUp");
 
-// import json
-const fs = require("fs");
-const path = require("path");
 const { updateDockerImages } = require("./src/services/mainService");
 const jsonPath = path.join(__dirname, "assets/fake-data.json");
 
@@ -43,140 +45,115 @@ exports.processContainerWritten = onDocumentWritten(
   processContainerWritten
 );
 
-const init = async () => {
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
-  // wait emulators to be ready
-  const maxRetries = 10;
-  let retries = 0;
-  let db;
-  while (retries < maxRetries) {
-    try {
-      db = admin.firestore();
-      await db.collection("test").doc("test").set({ test: "test" });
-      console.log("Emulators are ready");
-      break;
-    } catch (error) {
-      console.log("Waiting for emulators to be ready...", error);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      retries++;
-    }
-  }
-
-  // const db = admin.firestore();
+(async () => {
+  const db = admin.firestore();
   const batch = db.batch();
 
-  /*-------------------*/
-  /* Docker            */
-  /*-------------------*/
-  // wait for JWT_SECRET to be available
-  // let retries = 0;
-  // while (!process.env.JWT_SECRET && retries < 10) {
-  //   console.log("Waiting for JWT_SECRET...", process.env);
-  //   await new Promise((resolve) => setTimeout(resolve, 1000));
-  //   retries++;
-  // }
+  try {
+    /*-------------------*/
+    /* Docker            */
+    /*-------------------*/
+    console.log("Updating docker images...");
+    await updateDockerImages();
 
-  console.log("JWT", process.env.JWT_SECRET);
+    console.log("Creating fake data...");
 
-  await updateDockerImages();
+    /*-------------------*/
+    /* Statistics        */
+    /*-------------------*/
+    const statisticsRef = db.collection("statistics");
+    const types = [
+      "comments",
+      "info",
+      "users",
+      "totalReviews",
+      "totalImages",
+      "totalVideos",
+      "totalOwnerReviews",
+      "totalValidateComments",
+      "totalValidateInfo",
+      "earnings",
+    ];
+    for (const type of types) {
+      const ref = statisticsRef.doc(type);
+      const doc = await ref.get();
+      if (doc.exists) continue;
 
-  /*-------------------*/
-  /* Statistics        */
-  /*-------------------*/
-  const statisticsRef = db.collection("statistics");
-  const types = [
-    "comments",
-    "info",
-    "users",
-    "totalReviews",
-    "totalImages",
-    "totalVideos",
-    "totalOwnerReviews",
-    "totalValidateComments",
-    "totalValidateInfo",
-    "earnings",
-  ];
-  for (const type of types) {
-    const ref = statisticsRef.doc(type);
-    const doc = await ref.get();
-    if (doc.exists) continue;
-
-    batch.set(ref, {
-      total: 0,
-    });
-  }
-
-  /*-------------------*/
-  /* Settings          */
-  /*-------------------*/
-  const settingsPricesRef = db.doc("settings/prices");
-  const settingsPricesSnap = await settingsPricesRef.get();
-  if (!settingsPricesSnap.exists) {
-    batch.set(settingsPricesRef, {
-      image: 2,
-      video: 3,
-      response: 1,
-      review: 1,
-      validation: 3,
-    });
-  }
-
-  /*-------------------*/
-  /* Fake earnings     */
-  /*-------------------*/
-  const earningsRef = db.collection("earnings");
-  const earningsSnap = await earningsRef.get();
-  if (earningsSnap.empty) {
-    const months = 12;
-    const now = new Date();
-    const month = now.getMonth();
-    const year = now.getFullYear();
-    const earnings = [];
-    for (let i = 0; i < months; i++) {
-      const date = new Date(year, month - i, 1);
-      const timestamp = Timestamp.fromDate(date);
-      const amount = Math.floor(Math.random() * 14000) + 1000;
-      earnings.push({
-        amount,
-        createdAt: timestamp,
-        paymentId: `payment-${i}`,
-        userId: "user",
+      batch.set(ref, {
+        total: 0,
       });
     }
 
-    for (const earning of earnings) {
-      const ref = earningsRef.doc(earning.paymentId);
-      batch.set(ref, earning);
-    }
-
-    // Add the total earnings to statistics
-    const totalEarningsRef = db.doc("statistics/earnings");
-    const totalEarningsSnap = await totalEarningsRef.get();
-    if (!totalEarningsSnap.exists) {
-      batch.set(totalEarningsRef, {
-        total: earnings.reduce((acc, curr) => acc + curr.amount, 0),
+    /*-------------------*/
+    /* Settings          */
+    /*-------------------*/
+    const settingsPricesRef = db.doc("settings/prices");
+    const settingsPricesSnap = await settingsPricesRef.get();
+    if (!settingsPricesSnap.exists) {
+      batch.set(settingsPricesRef, {
+        image: 2,
+        video: 3,
+        response: 1,
+        review: 1,
+        validation: 3,
       });
     }
+
+    /*-------------------*/
+    /* Fake earnings     */
+    /*-------------------*/
+    const earningsRef = db.collection("earnings");
+    const earningsSnap = await earningsRef.get();
+    if (earningsSnap.empty) {
+      const months = 12;
+      const now = new Date();
+      const month = now.getMonth();
+      const year = now.getFullYear();
+      const earnings = [];
+      for (let i = 0; i < months; i++) {
+        const date = new Date(year, month - i, 1);
+        const timestamp = Timestamp.fromDate(date);
+        const amount = Math.floor(Math.random() * 14000) + 1000;
+        earnings.push({
+          amount,
+          createdAt: timestamp,
+          paymentId: `payment-${i}`,
+          userId: "user",
+        });
+      }
+
+      for (const earning of earnings) {
+        const ref = earningsRef.doc(earning.paymentId);
+        batch.set(ref, earning);
+      }
+
+      // Add the total earnings to statistics
+      const totalEarningsRef = db.doc("statistics/earnings");
+      const totalEarningsSnap = await totalEarningsRef.get();
+      if (!totalEarningsSnap.exists) {
+        batch.set(totalEarningsRef, {
+          total: earnings.reduce((acc, curr) => acc + curr.amount, 0),
+        });
+      }
+    }
+
+    /*-------------------*/
+    /* Fake data         */
+    /*-------------------*/
+    const data = JSON.parse(fs.readFileSync(jsonPath));
+    const containerRef = db.collection("containers");
+    for (const container of data) {
+      const ref = containerRef.doc(`${container.type}_${container.reviewId}`);
+      batch.set(ref, {
+        ...container,
+        createdAt: Timestamp.now(),
+        updatedAt: Timestamp.now(),
+        location: new GeoPoint(container.location[0], container.location[1]),
+      });
+    }
+
+    await batch.commit();
+  } catch (error) {
+    console.error(error);
   }
-
-  /*-------------------*/
-  /* Fake data         */
-  /*-------------------*/
-  const data = JSON.parse(fs.readFileSync(jsonPath));
-  const containerRef = db.collection("containers");
-  for (const container of data) {
-    const ref = containerRef.doc(`${container.type}_${container.reviewId}`);
-    batch.set(ref, {
-      ...container,
-      createdAt: Timestamp.now(),
-      updatedAt: Timestamp.now(),
-      location: new GeoPoint(container.location[0], container.location[1]),
-    });
-  }
-
-  await batch.commit();
-};
-
-setTimeout(() => {
-  init().catch(console.error);
-}, 10000);
+})();
--- codecanyon/gmr_scrap/demo/server/routes/dockerRoutes.js
+++ codecanyon/gmr_scrap/demo/server/routes/dockerRoutes.js
@@ -3,6 +3,13 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const { imagesDocker } = require("../controllers/dockerController");
 const router = express.Router();
 
-router.post("/images", authMiddleware, imagesDocker);
+router.post("/images", authMiddleware, async (req, res) => {
+  try {
+    await imagesDocker();
+    res.status(200).send("Images updated");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 module.exports = router;
