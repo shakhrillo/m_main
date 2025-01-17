f3c12af9da54e28edc180e92bae6e104e3084caf
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-01-17 17:51:45 +0500

    Remove Google Cloud VM instance handling and related constants from the project

--- codecanyon/gmr_scrap/demo/server/routes/scraperRoutes.js
+++ codecanyon/gmr_scrap/demo/server/routes/scraperRoutes.js
@@ -2,99 +2,30 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
 const { startContainer } = require("../controllers/dockerController");
-const { startVmInstance } = require("../controllers/googleController");
 
 const environment = process.env.NODE_ENV || "development";
 const machineBuildImageName =
   process.env.MACHINE_BUILD_IMAGE_NAME || "gmr_scrap_machine";
 const firebaseProjectId = process.env.FIREBASE_PROJECT_ID || "fir-scrapp";
 
-var os = require("os");
-// Get the network interfaces
-const networkInterfaces = os.networkInterfaces();
-
-// Find the first non-internal IPv4 address
-const firstIPAddress = Object.values(networkInterfaces)
-  .flat()
-  .find(
-    (iface) => iface && !iface.internal && iface.family === "IPv4"
-  )?.address;
-
 const handleContainerOperations = async (req, res) => {
   try {
-    console.log(
-      `First IP Address: ${firstIPAddress || "No external IPv4 address found"}`
-    );
-    console.log("Request data", req.data);
     const { tag, type } = req.data;
-
-    console.log(
-      "Starting container>>",
-      tag,
-      machineBuildImageName,
-      firebaseProjectId
-    );
-
-    if (environment === "production") {
-      if (tag.includes("info")) {
-        await startContainer({
-          Image: machineBuildImageName,
-          name: tag,
-          Env: [
-            `TAG=${tag}`,
-            `NODE_ENV=${environment}`,
-            `FIREBASE_PROJECT_ID=${firebaseProjectId}`,
-          ],
-          Cmd: ["npm", "run", "info"],
-          HostConfig: {
-            AutoRemove: true,
-          },
-        });
-      } else {
-        await startVmInstance({
-          tag,
-          startupScript: `
-            # Run the tests
-            cd /home/st/m_main/codecanyon/gmr_scrap/demo/selenium_machine
-  
-            #docker run \
-            #  --name ${tag} \
-            #  -e TAG=${tag} \
-            #  -e NODE_ENV=${environment} \
-            #  -e FIREBASE_PROJECT_ID=${firebaseProjectId} \
-            #  ${machineBuildImageName} \
-  
-            # Remove environment file
-            rm -f .env
-  
-            # Create environment file
-            echo "TAG=${tag}" > .env
-            echo "NODE_ENV=${environment}" >> .env
-            echo "FIREBASE_PROJECT_ID=${firebaseProjectId}" >> .env
-  
-            ${isInfo ? "npm run info" : "npm run start"}
-          `,
-        });
-      }
-    }
-
-    if (environment === "development") {
-      await startContainer({
-        Image: machineBuildImageName,
-        name: tag,
-        Env: [
-          `TAG=${tag}`,
-          `NODE_ENV=${environment}`,
-          `FIREBASE_PROJECT_ID=${firebaseProjectId}`,
-          `FIREBASE_URL=${process.env.FIREBASE_IP}`,
-          `CHROME_PATH=/usr/bin/chromium-browser`,
-        ],
-        Cmd: ["npm", "run", type],
-        HostConfig: {
-          AutoRemove: !true,
-        },
-      });
-    }
+    await startContainer({
+      Image: machineBuildImageName,
+      name: tag,
+      Env: [
+        `TAG=${tag}`,
+        `NODE_ENV=${environment}`,
+        `FIREBASE_PROJECT_ID=${firebaseProjectId}`,
+        `FIREBASE_URL=${process.env.FIREBASE_IP}`,
+        `CHROME_PATH=/usr/bin/chromium-browser`,
+      ],
+      Cmd: ["npm", "run", type],
+      HostConfig: {
+        AutoRemove: !true,
+      },
+    });
 
     res.json({ message: "Started" });
   } catch (error) {
@@ -106,9 +37,5 @@ const handleContainerOperations = async (req, res) => {
 router.post("/", authMiddleware, async (req, res) =>
   handleContainerOperations(req, res)
 );
-
-// router.post("/info", authMiddleware, async (req, res) =>
-//   handleContainerOperations(req, res, true)
-// );
 
 module.exports = router;
