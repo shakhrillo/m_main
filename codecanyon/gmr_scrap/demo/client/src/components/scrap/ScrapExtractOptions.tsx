1279716f68c2da3874eeb962c481660c175905df
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-01-21 22:57:57 +0500

    Refactor ScrapExtractOptions component: simplify extraction options display and enhance layout; update descriptions for clarity and brevity.

--- codecanyon/gmr_scrap/demo/client/src/components/scrap/ScrapExtractOptions.tsx
+++ codecanyon/gmr_scrap/demo/client/src/components/scrap/ScrapExtractOptions.tsx
@@ -1,28 +1,29 @@
 import {
+  IconArrowsSort,
   IconCheck,
-  IconCircleDashed,
+  IconFence,
+  IconFile,
   IconMessageReply,
   IconPhoto,
   IconVideo,
 } from "@tabler/icons-react";
-import { createElement, useState } from "react";
+import { createElement } from "react";
 import {
+  Badge,
   Card,
   CardBody,
-  Col,
-  FormCheck,
-  FormGroup,
+  FormControl,
   FormLabel,
-  Row,
-  Stack,
+  FormSelect,
+  FormText,
 } from "react-bootstrap";
-import FormCheckLabel from "react-bootstrap/esm/FormCheckLabel";
 
+// Define extraction options
 const EXTRACT_OPTIONS = [
   {
     title: "Image URLs",
     description:
-      "Extract image URLs from reviews, assigning 2 points to each URL. Accurately count and total the points based on the number of images found.",
+      "Extract image URLs from reviews. Assigns 2 points to each URL.",
     points: 2,
     id: "extractImageUrls",
     icon: IconPhoto,
@@ -30,7 +31,7 @@ const EXTRACT_OPTIONS = [
   {
     title: "Video URLs",
     description:
-      "Extract video URLs from reviews, assigning 3 points to each URL. Accurately count and total the points based on the number of videos found.",
+      "Extract video URLs from reviews. Assigns 3 points to each URL.",
     points: 3,
     id: "extractVideoUrls",
     icon: IconVideo,
@@ -38,65 +39,85 @@ const EXTRACT_OPTIONS = [
   {
     title: "Owner response",
     description:
-      "Extract owner responses from reviews, assigning 1 point to each response. Accurately count and total the points based on the number of responses found.",
+      "Extract owner responses from reviews. Assigns 1 point to each response.",
     points: 1,
     id: "extractOwnerResponse",
     icon: IconMessageReply,
   },
 ];
+
 export const ScrapExtractOptions = () => {
-  const [extractOptions, setExtractOptions] = useState({
-    extractImageUrls: false,
-    extractVideoUrls: false,
-    extractOwnerResponse: false,
-  } as Record<string, boolean>);
-
   return (
-    <FormGroup controlId="extractOptions">
-      <FormLabel className="form-label">Options</FormLabel>
-      <Row className="g-3">
-        {EXTRACT_OPTIONS.map((option, index) => (
-          <Col lg={6} xxl={4} key={index}>
-            <Card>
-              <CardBody>
-                <FormCheck
-                  type="checkbox"
-                  className="position-absolute visually-hidden"
-                  id={option.id}
-                  value={option.id}
-                />
-                <FormCheckLabel htmlFor={option.id}>
-                  <span
-                    className={
-                      "position-absolute top-0 end-0 rounded-circle mt-n2 me-n2 border bg-warning " +
-                      (extractOptions[option.id] === true
-                        ? "border-primary"
-                        : "border-light-subtle")
-                    }
-                  >
-                    {extractOptions[option.id] === true ? (
-                      <IconCheck size={20} className="m-1" />
-                    ) : (
-                      <IconCircleDashed size={20} className="m-1" />
-                    )}
-                  </span>
+    <>
+      {EXTRACT_OPTIONS.map((option) => (
+        <Card key={option.id}>
+          <CardBody className="d-flex align-items-start gap-3">
+            {createElement(option.icon, {
+              className: "text-body-secondary",
+            })}
+            <span className="d-inline">
+              {option.title}
+              <Badge bg="secondary" className="ms-2">
+                {option.points} points
+              </Badge>
+            </span>
+            <IconCheck className="ms-auto" />
+          </CardBody>
+        </Card>
+      ))}
 
-                  <Stack direction="horizontal" gap={3}>
-                    {createElement(option.icon, {
-                      size: 48,
-                      className: "text-body-secondary",
-                    })}
-                    <div>
-                      <h5 className="m-0">{option.title}</h5>
-                      <div className="badge bg-secondary">+23 points</div>
-                    </div>
-                  </Stack>
-                </FormCheckLabel>
-              </CardBody>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </FormGroup>
+      <Card>
+        <CardBody className="d-flex align-items-start gap-3">
+          {createElement(IconFence, {
+            className: "text-body-secondary",
+          })}
+          <div className="w-100">
+            <FormLabel>Review limit</FormLabel>
+            <FormControl placeholder="Review limit" />
+            <FormText>
+              Limit the number of reviews to extract. Leave empty to extract all
+              reviews.
+            </FormText>
+          </div>
+        </CardBody>
+      </Card>
+
+      <Card>
+        <CardBody className="d-flex align-items-start gap-3">
+          {createElement(IconArrowsSort, {
+            className: "text-body-secondary",
+          })}
+          <div className="w-100">
+            <FormLabel>Sorts by</FormLabel>
+            <FormSelect>
+              <option value="most-relevant">Most relevant</option>
+              <option value="newest">Newest</option>
+              <option value="highest-rating">Highest rating</option>
+              <option value="lowest-rating">Lowest rating</option>
+            </FormSelect>
+            <FormText>
+              Sort reviews by most relevant, newest, highest rating, or lowest
+              rating.
+            </FormText>
+          </div>
+        </CardBody>
+      </Card>
+
+      <Card>
+        <CardBody className="d-flex align-items-start gap-3">
+          {createElement(IconFile, {
+            className: "text-body-secondary",
+          })}
+          <div className="w-100">
+            <FormLabel>Output as</FormLabel>
+            <FormSelect>
+              <option value="json">JSON</option>
+              <option value="csv">CSV</option>
+            </FormSelect>
+            <FormText>Output as a CSV or JSON file.</FormText>
+          </div>
+        </CardBody>
+      </Card>
+    </>
   );
 };
