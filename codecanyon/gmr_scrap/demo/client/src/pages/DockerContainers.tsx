8b9fd3b050623eb9d26fbe7031c4083b6c3dab90
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-01-26 20:26:10 +0500

    Refactor DockerContainers, ReviewsList, and ValidatedUrls components: remove unused code and replace card tables with ContainersList for improved maintainability and clarity.

--- codecanyon/gmr_scrap/demo/client/src/pages/DockerContainers.tsx
+++ codecanyon/gmr_scrap/demo/client/src/pages/DockerContainers.tsx
@@ -1,67 +1,10 @@
-import { IconCheck, IconSearch } from "@tabler/icons-react";
-import { User } from "firebase/auth";
-import { useEffect, useState } from "react";
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  Col,
-  Container,
-  Dropdown,
-  FormControl,
-  InputGroup,
-  Row,
-  Stack,
-  Table,
-} from "react-bootstrap";
-import { NavLink, useOutletContext } from "react-router-dom";
-import { Statistics } from "../components/Statistics";
-import { dockerContainers } from "../services/dockerService";
-import { IDockerContainer } from "../types/dockerContainer";
-import { formatDate } from "../utils/formatDate";
+import { Col, Container, Row } from "react-bootstrap";
 import { ContainersList } from "../components/containers/ContainersList";
-const FILTER_OPTIONS = [
-  { value: "", label: "All" },
-  { value: "pending", label: "Pending" },
-  { value: "error", label: "Failed" },
-];
 
 export const DockerContainers = () => {
-  const { uid } = useOutletContext<User>();
-  const [containers, setContainers] = useState<IDockerContainer[]>([]);
-  const [search, setSearch] = useState("");
-  const [filter, setFilter] = useState("");
-
-  const stats = [
-    {
-      label: "Total Containers",
-      icon: IconCheck,
-      value: 10,
-    },
-  ];
-
-  useEffect(() => {
-    const subscription = dockerContainers().subscribe((data) => {
-      console.log("___", data);
-      setContainers(data);
-    });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, [uid]);
-
   return (
     <Container>
       <Row className="g-3">
-        {/*---Extracted reviews status---*/}
-        {stats.map((stat, index) => (
-          <Col key={index}>
-            <Statistics {...stat} />
-          </Col>
-        ))}
-        {/*---End: Extracted reviews status---*/}
-
         <Col xs={12}>
           <ContainersList path="containers" />
         </Col>
--- codecanyon/gmr_scrap/demo/client/src/pages/ValidatedUrls.tsx
+++ codecanyon/gmr_scrap/demo/client/src/pages/ValidatedUrls.tsx
@@ -1,76 +1,9 @@
-import { IconInfoCircle, IconSearch } from "@tabler/icons-react";
-import { User } from "firebase/auth";
-import { useEffect, useState } from "react"; // React imports for state and effect handling
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  Col,
-  Container,
-  Dropdown,
-  Form,
-  InputGroup,
-  Row,
-  Stack,
-  Table,
-} from "react-bootstrap";
-import { useNavigate, useOutletContext } from "react-router-dom"; // Hook for navigation
-import { Statistics } from "../components/Statistics";
-import { StatusInfo } from "../components/StatusInfo";
-import { IReview, validatedUrls } from "../services/scrapService";
-import { userData } from "../services/userService";
-import { formatTimestamp } from "../utils/formatTimestamp"; // Utility to format timestamps
-import { dockerContainers } from "../services/dockerService";
-import { IDockerContainer } from "../types/dockerContainer";
+import { Col, Container, Row } from "react-bootstrap";
 import { ContainersList } from "../components/containers/ContainersList";
-const FILTER_OPTIONS = [
-  { value: "", label: "All" },
-  { value: "pending", label: "Pending" },
-  { value: "error", label: "Failed" },
-];
 export const ValidatedURLs = () => {
-  const { uid } = useOutletContext<User>();
-  const navigate = useNavigate();
-
-  const [info, setInfo] = useState<any>({});
-  const [reviews, setReviews] = useState<IDockerContainer[]>([]);
-  const [search, setSearch] = useState("");
-  const [filter, setFilter] = useState("");
-
-  const stats = [
-    {
-      label: "Total Validates",
-      icon: IconInfoCircle,
-      value: info.totalValidateInfo || "0",
-    },
-  ];
-
-  useEffect(() => {
-    const subscription = dockerContainers({
-      type: "info",
-    }).subscribe((data) => {
-      setReviews(data);
-    });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, [search, uid, filter]);
-
-  useEffect(() => {
-    const subscription = userData(uid).subscribe((data) => {
-      setInfo(data);
-    });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, [uid]);
-
   return (
     <Container>
       <Row className="g-3">
-        {/* Table for displaying reviews based on active filter */}
         <Col xs={12}>
           <ContainersList path="scrap" type="info" />
         </Col>
