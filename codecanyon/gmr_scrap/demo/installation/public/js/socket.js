60fef3a915cba0830f05369f3b32de5875b6c033
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-02-02 12:04:36 +0500

    Refactor CSS and JavaScript: update styles for dockerStats, simplify log handling in socket.js, and clean up button action logic in machine.js

--- codecanyon/gmr_scrap/demo/installation/public/js/socket.js
+++ codecanyon/gmr_scrap/demo/installation/public/js/socket.js
@@ -1,41 +1,19 @@
 const socket = io();
-let logsDockerBuild = [];
 
-/**
- * Update logs in the DOM.
- * @param {string} selector - The selector of the container.
- * @param {Array} logs - The logs array.
- * @param {string} log - The log to add.
- * @returns {void}
- */
-const updateLogs = (selector, logs, log) => {
+const updateLogs = (selector, log) => {
   const container = document.querySelector(selector);
   if (!container) return;
 
   const isUserAtBottom =
     container.scrollHeight - container.scrollTop === container.clientHeight;
-
-  // logs.push(log);
-
-  // const fragment = document.createDocumentFragment();
-  // const logItem = document.createElement("span");
-  // logItem.className = "d-block small";
-  // logItem.textContent = log;
-  // fragment.appendChild(document.createTextNode(`${log}\n`));
-
-  // container.appendChild(fragment);
-
   container.textContent += log + "\n";
 
   if (isUserAtBottom) {
     container.scrollTop = container.scrollHeight;
   }
 
-  // const codeElement = document.getElementById("dockerStats");
   delete container.dataset.highlighted;
   hljs.highlightElement(container);
 };
 
-socket.on("docker-build", (log) =>
-  updateLogs("#dockerStats", logsDockerBuild, log)
-);
+socket.on("docker-build", (log) => updateLogs("#dockerStats", log));
