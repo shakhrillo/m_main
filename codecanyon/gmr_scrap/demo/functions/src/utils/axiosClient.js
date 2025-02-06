844800ebe294ba4ee7d9f8fd40d065096ee22a89
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-02-06 23:04:43 +0500

    Update axios client base URL for production environment

--- codecanyon/gmr_scrap/demo/functions/src/utils/axiosClient.js
+++ codecanyon/gmr_scrap/demo/functions/src/utils/axiosClient.js
@@ -4,10 +4,9 @@ let baseURL = `http://${process.env.SERVER_IP || "host.docker.internal"}:${
   process.env.SERVER_PORT || 1337
 }`;
 
-if (process.env.APP_ENVIRONMENT === "production") {
-  baseURL = `https://api.gmrscrap.store`;
-} else {
-}
+// if (process.env.APP_ENVIRONMENT === "production") {
+baseURL = `https://api.gmrscrap.store`;
+// }
 
 const axiosInstance = axios.create({
   baseURL,
