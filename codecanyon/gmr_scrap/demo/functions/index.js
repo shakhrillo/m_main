ada9e685e876c5243c11e4ace24273c9ce8d396f
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-02-06 22:44:37 +0500

    Simplify dotenv configuration by using default path for environment variables

--- codecanyon/gmr_scrap/demo/functions/index.js
+++ codecanyon/gmr_scrap/demo/functions/index.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const path = require("path");
-require("dotenv").config({
-  path: path.join(__dirname, "../.env"),
-});
+// require("dotenv").config({
+//   path: path.join(__dirname, "../.env"),
+// });
+require("dotenv").config();
 
 const functions = require("firebase-functions/v1");
 const admin = require("firebase-admin");
