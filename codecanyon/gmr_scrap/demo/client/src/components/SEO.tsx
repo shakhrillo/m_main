d116ad02bd0fe3a846a5dca9d2842296db05f005
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-02-14 19:47:08 +0500

    Enhance SEO component to manage dynamic keywords and descriptions; update Helmet usage for improved metadata handling.

--- codecanyon/gmr_scrap/demo/client/src/components/SEO.tsx
+++ codecanyon/gmr_scrap/demo/client/src/components/SEO.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
 import { filter } from "rxjs";
 import { settingValue } from "../services/settingService";
-import { Helmet } from "react-helmet";
 
 /**
  * SEO component.
@@ -9,20 +9,37 @@ import { Helmet } from "react-helmet";
  */
 export const SEO = () => {
     const [title, setTitle] = useState<string | null>(null);
+    const [keywords, setKeywords] = useState<string | null>(null);
+    const [description, setDescription] = useState<string | null>(null);
 
     useEffect(() => {
         const subscription = settingValue({ tag: "title", type: "general" })
             .pipe(filter((data) => !!data?.value))
-            .subscribe(({ value }) => {
-                console.log(value);
-                setTitle(value)
-            });
+            .subscribe(({ value }) => setTitle(value));
+
+        return () => subscription.unsubscribe();
+    }, []);
+
+    useEffect(() => {
+        const subscription = settingValue({ tag: "keywords", type: "general" })
+            .pipe(filter((data) => !!data?.value))
+            .subscribe(({ value }) => setKeywords(value));
+
+        return () => subscription.unsubscribe();
+    }, []);
+
+    useEffect(() => {
+        const subscription = settingValue({ tag: "description", type: "general" })
+            .pipe(filter((data) => !!data?.value))
+            .subscribe(({ value }) => setDescription(value));
 
         return () => subscription.unsubscribe();
     }, []);
 
     return <Helmet>
         <title>{ title }</title>
-        <meta name="description" content="This is a dynamic page." />
+        <meta charSet="utf-8" />
+        <meta name="keywords" content={ keywords || "" } />
+        <meta name="description" content={ description || "" } />
     </Helmet>
 };
