c4013e1f5a5fb1c961396c202249d05bf74f9fa9
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-02-18 00:41:42 +0500

    Refactor initial data import logic in firebase-deploy.js and update initial-data.json structure

--- codecanyon/gmr_scrap/demo/firebase-deploy.js
+++ codecanyon/gmr_scrap/demo/firebase-deploy.js
@@ -1,30 +1,40 @@
 const admin = require('firebase-admin');
 const fs = require('fs');
-const APP_FIREBASE_PROJECT_ID = process.env.APP_FIREBASE_PROJECT_ID;
-const GOOGLE_APPLICATION_CREDENTIALS = '/usr/src/app/firebaseServiceAccount.json';
-const INITIAL_DATA_PATH = '/usr/src/app/initial-data.json';
+
+const FIREBASE_PROJECT_ID = process.env.APP_FIREBASE_PROJECT_ID;
+const FIREBASE_CREDENTIALS_PATH = '/usr/src/app/firebaseServiceAccount.json';
+const INITIAL_DATA_FILE_PATH = '/usr/src/app/initial-data.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(GOOGLE_APPLICATION_CREDENTIALS),
-  projectId: APP_FIREBASE_PROJECT_ID,
+  credential: admin.credential.cert(FIREBASE_CREDENTIALS_PATH),
+  projectId: FIREBASE_PROJECT_ID,
 });
 
 const firestore = admin.firestore();
 
-
-(async () => {
-  if (fs.existsSync(INITIAL_DATA_PATH)) {
-    const initialData = require(INITIAL_DATA_PATH);
-    const batch = firestore.batch();
-    
-    initialData.forEach((data) => {
-      const collectionRef = firestore.collection(data.collection).doc(data.documentId);
-      batch.set(collectionRef, data.data);
-    });
-
-    await batch.commit();
-    console.log('Initial data imported.');
-  } else {
-    console.error('Initial data file not found.');
+const importInitialData = async () => {
+  if (!fs.existsSync(INITIAL_DATA_FILE_PATH)) {
+    console.error('Error: Initial data file not found.');
+    return;
   }
-})();
+
+  const initialData = require(INITIAL_DATA_FILE_PATH);
+  const batch = firestore.batch();
+
+  initialData.forEach((data) => {
+    const fields = data.fields;
+    const type = data.type;
+    fields.forEach((field) => {
+      const documentRef = firestore.collection('settings').doc();
+      batch.set(documentRef, {
+        ...field,
+        type,
+      });
+    });
+  });
+
+  await batch.commit();
+  console.log('✅ Initial data import successful.');
+};
+
+importInitialData();
