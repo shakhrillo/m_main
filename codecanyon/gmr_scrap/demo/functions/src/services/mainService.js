e55d6980f045bc5ff17f29f826d85de7801ddf0b
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-02-18 08:35:08 +0500

    Refactor mainService to use axios instance and improve API calls with token handling

--- codecanyon/gmr_scrap/demo/functions/src/services/mainService.js
+++ codecanyon/gmr_scrap/demo/functions/src/services/mainService.js
@@ -1,73 +1,55 @@
-const axiosInstance = require("../utils/axiosClient");
+// const axiosInstance = require("../utils/axiosClient");
 const { createToken } = require("../utils/jwtUtils");
+const axios = require("axios");
+
+let baseURL = `http://${process.env.SERVER_IPV4_ADDRESS}:${process.env.APP_SERVER_PORT}`;
+if (process.env.APP_ENVIRONMENT === "production") {
+  baseURL = process.env.API_PRODUCTION_URL;
+}
+
+const api = axios.create({ baseURL });
 
 /**
  * Execute scraping
  * @typedef {{ tag: string, type: string }} Tag
  * @param {Tag} data
+ * @returns {Promise<void>}
  */
 const executeScraping = async (data) => {
   const token = createToken(data);
-
-  const headers = {
-    Authorization: `Bearer ${token}`,
-  };
-
-  const config = {
-    headers,
-    method: "POST",
-  };
-
-  await axiosInstance.post(`/scrap`, {}, config);
+  console.log("token", token);
+  await api.post(`/scrap`, {}, { headers: { Authorization: `Bearer ${token}` } });
 };
 
 /**
- * Generate stripe payment url
+ * Generate Stripe payment URL
  * @typedef {{ userId: string, amount: number }} PaymentData
  * @param {PaymentData} data
  * @returns {Promise<string>}
  */
 const generateStripePaymentUrl = async (data) => {
   const token = createToken(data);
-
-  const headers = {
-    Authorization: `Bearer ${token}`,
-  };
-
-  const config = {
-    headers,
-  };
-
-  const results = await axiosInstance.post(`/stripe`, {}, config);
-  return results.data.url;
+  const response = await api.post(`/stripe`, {}, { headers: { Authorization: `Bearer ${token}` } });
+  return response.data.url;
 };
 
+/**
+ * Update Docker images
+ * @returns {Promise<void>}
+ */
 const updateDockerImages = async () => {
   const token = createToken({});
-
-  const headers = {
-    Authorization: `Bearer ${token}`,
-  };
-
-  const config = {
-    headers,
-  };
-
-  await axiosInstance.post(`/docker/images`, {}, config);
+  await api.post(`/docker/images`, {}, { headers: { Authorization: `Bearer ${token}` } });
 };
 
+/**
+ * Get Docker info
+ * @returns {Promise<object>}
+ */
 const getDockerInfo = async () => {
   const token = createToken({});
-
-  const headers = {
-    Authorization: `Bearer ${token}`,
-  };
-
-  const config = {
-    headers,
-  };
-
-  await axiosInstance.get(`/docker/info`, config);
+  const response = await api.get(`/docker/info`, { headers: { Authorization: `Bearer ${token}` } });
+  return response.data;
 };
 
 module.exports = {
