8183cfc26c7be6774171f92462d3a97321a4dff3
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-02-21 12:48:05 +0500

    Refactor Firebase functions: update document event triggers, upgrade dependencies, and add export data files

--- codecanyon/gmr_scrap/demo/functions/index.js
+++ codecanyon/gmr_scrap/demo/functions/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const functions = require("firebase-functions/v1");
 const admin = require("firebase-admin");
-const { onDocumentCreated, onDocumentWritten } = require("firebase-functions/v2/firestore");
+const { onDocumentCreated, onDocumentUpdated } = require("firebase-functions/v2/firestore");
 const processBuyCoins = require("./src/payments/processBuyCoins");
 const processUserCreated = require("./src/user/processUserCreated");
 const processContainerCreated = require("./src/containers/processContainerCreated");
@@ -15,4 +15,4 @@ exports.processUserCreated = functions.auth.user().onCreate(processUserCreated);
 exports.processBuyCoins = onDocumentCreated("users/{userId}/buyCoins/{coinId}", processBuyCoins);
 exports.userTopUp = onDocumentCreated("payments/{paymentId}", userTopUp);
 exports.processContainerCreated = onDocumentCreated("containers/{containerId}", processContainerCreated);
-exports.processMachineWritten = onDocumentWritten("machines/{machineId}", processMachineWritten);
+exports.processMachineWritten = onDocumentUpdated("machines/{machineId}", processMachineWritten);
