662187ab95e5321ef5d6d9dafc09850912da5173
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-03-02 02:52:17 +0500

    Refactor ContainerLogs component: optimize log fetching with useCallback, enhance log handling, and improve subscription management for better performance

--- codecanyon/gmr_scrap/demo/client/src/components/containers/ContainerLogs.tsx
+++ codecanyon/gmr_scrap/demo/client/src/components/containers/ContainerLogs.tsx
@@ -1,41 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { dockerContainerLogs } from "../../services/dockerService";
 import { IconChevronRight } from "@tabler/icons-react";
 
 interface IContainerLogs {
-  containerId: string | undefined;
+  containerId?: string;
 }
 
 export const ContainerLogs = ({ containerId }: IContainerLogs) => {
   const [logs, setLogs] = useState<string[]>([]);
 
-  useEffect(() => {
-    if (!containerId) {
-      return;
-    }
+  const fetchLogs = useCallback(() => {
+    if (!containerId) return;
 
     const subscription = dockerContainerLogs(containerId).subscribe({
-      next: (data) => {
-        if (!data || data.length === 0) {
-          return;
-        }
-
-        setLogs(data.map(({ logs }: { logs: string }) => logs));
+      next: (data = []) => {
+        setLogs((prevLogs) => [...prevLogs, ...data.flatMap(({ logs }: { logs: string }) => logs.trim())]);
       },
-      error: (error) => console.error("Error fetching logs data:", error),
+      error: (error) => console.error("Error fetching logs:", error),
     });
 
-    return () => {
-      subscription.unsubscribe();
-    };
+    return () => subscription.unsubscribe();
   }, [containerId]);
+
+  useEffect(fetchLogs, [fetchLogs]);
 
   return (
     <div className="container-logs">
       {logs.map((log, index) => (
         <div className="container-log" key={index}>
           <IconChevronRight size={16} />
-          {log.trim().replace(/\n/g, "")}
+          {log}
         </div>
       ))}
     </div>
