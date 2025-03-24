fb8ffb18df3b976d63be49572a6c0aa933002bce
Mukhammad Tukhsanov <muxammadhotamo@gmail.com>
2025-03-24 14:36:57 +0500

    new changes

--- landing-page/script.js
+++ landing-page/script.js
@@ -6,13 +6,43 @@ function scrollPricingContent(distance) {
 function scrollContent(distance) {
   scrollContainer.scrollLeft += distance; // Scrolls by the specified distance
 }
-
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
   if (window.scrollY > 50) {
-    navbar.classList.add("scrolled");
+    navbar.classList.add("scrolled", "bg-white");
+    navbar.classList.remove("bg-transparent", "ser");
   } else {
-    console.log("Sdasd");
-    navbar.classList.remove("scrolled");
+    navbar.classList.remove("scrolled", "bg-white");
+    navbar.classList.add("bg-transparent", "ser");
   }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".navbar");
+  const logo = document.getElementById("logo");
+
+  function updateLogo() {
+    if (!navbar.classList.contains("ser")) {
+      navbar.classList.add("ser");
+    }
+    logo.src = navbar.classList.contains("ser")
+      ? "./icons/logo-wing-white.svg"
+      : "./icons/logo-wing.svg";
+  }
+
+  updateLogo();
+
+  // Observe class changes
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.attributeName === "class") {
+        if (!navbar.classList.contains("ser")) {
+          navbar.classList.add("ser");
+        }
+        updateLogo();
+      }
+    });
+  });
+
+  observer.observe(navbar, { attributes: true, attributeFilter: ["class"] });
 });
