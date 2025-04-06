6f2cf90a4eedf03310e94ce79290682f2f0fef73
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-04-06 05:51:54 +0500

    Remove headless Chrome setup from IndeedJobScraper

--- indeedio/indeed_scraper.py
+++ indeedio/indeed_scraper.py
@@ -14,13 +14,13 @@ class IndeedJobScraper(BaseCase):
         template = 'https://www.indeed.com/jobs?q={}&l={}&start={}'
         return template.format(position, location, page_number * 10)
 
-    def setUp(self):
-        # Setting up headless options for Chrome WebDriver
-        options = Options()
-        options.add_argument("--headless")  # Ensures Chrome is running in headless mode
-        options.add_argument("--disable-gpu")  # Disables GPU hardware acceleration
-        options.add_argument("--window-size=1920x1080")  # Simulates a full-size window
-        self.driver = self.get_new_driver(options=options)  # Use headless Chrome
+    # def setUp(self):
+    #     # Setting up headless options for Chrome WebDriver
+    #     options = Options()
+    #     options.add_argument("--headless")  # Ensures Chrome is running in headless mode
+    #     options.add_argument("--disable-gpu")  # Disables GPU hardware acceleration
+    #     options.add_argument("--window-size=1920x1080")  # Simulates a full-size window
+    #     self.driver = self.get_new_driver(options=options)  # Use headless Chrome
 
     def test_scrape_jobs(self, position='software developer', location='new york'):
         url = self.get_url(position, location,)
